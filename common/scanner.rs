90d2ab6b0597fff79b9a154d0eb0df0ba7a0d840
Kenko Nakamura <kenkou.n@gmail.com>
2018-02-21 02:52:32 +0900

    Update Scanner

--- common/scanner.rs
+++ common/scanner.rs
@@ -1,25 +1,52 @@
 struct Scanner {
-    buffer: std::collections::VecDeque<String>,
+    ptr: usize,
+    length: usize,
+    buf: Vec<u8>,
+    small_cache: Vec<u8>,
 }
 
 impl Scanner {
     fn new() -> Scanner {
-        Scanner { buffer: std::collections::VecDeque::new() }
+        Scanner { ptr: 0, length: 0, buf: vec![0; 1024], small_cache: vec![0; 1024] }
     }
 
-    fn read_line() -> String {
-        let stdin = std::io::stdin();
-        let mut buf = String::new();
-        stdin.read_line(&mut buf).unwrap();
-        buf
+    fn load(&mut self) {
+        use std::io::Read;
+        let mut s = std::io::stdin();
+        self.length = s.read(&mut self.buf).unwrap();
     }
+
+    fn read_byte(&mut self) -> u8 {
+        if self.ptr >= self.length {
+            self.ptr = 0;
+            self.load();
+        }
+        self.ptr += 1;
+        return self.buf[self.ptr - 1];
+    }
+
+    fn is_space(b: u8) -> bool { b == b'\n' || b == b'\r' || b == b'\t' || b == b' ' }
 
     fn read<T>(&mut self) -> T where T: std::str::FromStr, T::Err: std::fmt::Debug, {
-        if self.buffer.is_empty() {
-            for s in Scanner::read_line().split(' ') {
-                self.buffer.push_back(s.to_string());
+        let mut b = self.read_byte();
+        while Scanner::is_space(b) {
+            b = self.read_byte();
+        }
+
+        for pos in 0..self.small_cache.len() {
+            self.small_cache[pos] = b;
+            b = self.read_byte();
+            if Scanner::is_space(b) {
+                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)]).parse().unwrap();
             }
         }
-        self.buffer.pop_front().unwrap().trim().parse().unwrap()
+
+        let mut v = self.small_cache.clone();
+        while !Scanner::is_space(b) {
+            v.push(b);
+            b = self.read_byte();
+        }
+        return String::from_utf8_lossy(&v).parse().unwrap();
     }
 }
+
