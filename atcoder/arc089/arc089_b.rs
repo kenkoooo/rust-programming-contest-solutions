30bf026f579de8cea136c8985f05db7abd38b963
Kenko Nakamura <kenkou.n@gmail.com>
2018-02-25 22:49:46 +0900

    Review arc089_b

--- atcoder/arc089/arc089_b.rs
+++ atcoder/arc089/arc089_b.rs
@@ -1,95 +1,55 @@
-use std::io;
 use std::cmp;
 
 fn main() {
-    let (n, k) = {
-        let v = read_values::<usize>();
-        (v[0], v[1])
-    };
-
+    let mut sc = Scanner::new();
+    let n = sc.read::<usize>();
+    let k = sc.read::<usize>();
     let xyc = (0..n).map(|_| {
-        let v = read_values::<String>();
-        let x = v[0].parse::<usize>().unwrap();
-        let y = v[1].parse::<usize>().unwrap();
-        let c = (v[2] == "B");
-        if !c {
-            assert_eq!(v[2], "W");
-        }
-        (x % (2 * k), y % (2 * k), c)
+        let x = sc.read::<usize>() % (2 * k);
+        let y = sc.read::<usize>() % (2 * k);
+        let c = sc.read::<String>() == "B";
+        (x, y, c)
     }).collect::<Vec<_>>();
-    println!("{}", solve(&xyc, k));
-}
 
-fn solve(xyc: &Vec<(usize, usize, bool)>, k: usize) -> usize {
-    let n = xyc.len();
-    let mut b = vec![vec![0; k]; k];
-    let mut w = vec![vec![0; k]; k];
+    let (black, white) = {
+        let mut b = vec![vec![0; k]; k];
+        let mut w = vec![vec![0; k]; k];
+        for t in &xyc {
+            let (x, y, mut c) = *t;
+            if (x < k && y < k) || (x >= k && y >= k) {
+                c = !c;
+            }
 
-    for i in 0..n {
-        let (x, y, c) = xyc[i];
-
-        if x >= k && y >= k {
             if c {
                 b[x % k][y % k] += 1;
             } else {
                 w[x % k][y % k] += 1;
             }
-        } else if x >= k {
-            if c {
-                w[x % k][y] += 1;
-            } else {
-                b[x % k][y] += 1;
-            }
-        } else if y >= k {
-            if c {
-                w[x][y % k] += 1;
-            } else {
-                b[x][y % k] += 1;
-            }
-        } else {
-            if c {
-                b[x][y] += 1;
-            } else {
-                w[x][y] += 1;
-            }
         }
-    }
+        let black = CumulativeSum::new(&b);
+        let white = CumulativeSum::new(&w);
+        (black, white)
+    };
 
-    let b_sum = CumulativeSum::new(&b);
-    let w_sum = CumulativeSum::new(&w);
-
-    let w_all = w_sum.get_sum(0, 0, k - 1, k - 1);
-    let b_all = b_sum.get_sum(0, 0, k - 1, k - 1);
-    assert_eq!(w_all + b_all, n);
-
-    let mut ans = cmp::max(w_all, b_all);
+    let mut ans = 0;
     for i in 0..k {
         for j in 0..k {
-            let b_up = b_sum.get_sum(i, j, k - 1, k - 1);
-            let b_low = if i == 0 || j == 0 {
-                0
-            } else {
-                b_sum.get_sum(0, 0, i - 1, j - 1)
-            };
+            let b1 = black.get_sum(0, 0, i, j);
+            let b2 = black.get_sum(i + 1, j + 1, k - 1, k - 1);
 
-            let w_up = w_sum.get_sum(i, j, k - 1, k - 1);
-            let w_low = if i == 0 || j == 0 {
-                0
-            } else {
-                w_sum.get_sum(0, 0, i - 1, j - 1)
-            };
+            let w1 = white.get_sum(0, 0, i, j);
+            let w2 = white.get_sum(i + 1, j + 1, k - 1, k - 1);
 
-            let ok = b_up + b_low + w_all - w_up - w_low;
-            assert!(w_all >= w_up + w_low);
-            ans = cmp::max(ans, ok);
-
-            let ok = w_up + w_low + (n - w_all) - b_up - b_low;
-            ans = cmp::max(ans, ok);
+            let bc = b1 + b2 + white.get_sum(0, 0, k - 1, k - 1) - w1 - w2;
+            let wc = w1 + w2 + black.get_sum(0, 0, k - 1, k - 1) - b1 - b2;
+            ans = cmp::max(ans, bc);
+            ans = cmp::max(ans, wc);
         }
     }
-    assert!(ans <= n);
-    ans
+
+    println!("{}", ans);
 }
+
 
 struct CumulativeSum {
     ny: usize,
@@ -109,6 +69,7 @@ impl CumulativeSum {
         }
         CumulativeSum { ny: ny, nx: nx, sum: sum }
     }
+
     fn get_sum(&self, y1: usize, x1: usize, y2: usize, x2: usize) -> usize {
         if y1 > y2 || x1 > x2 {
             return 0;
@@ -119,16 +80,55 @@ impl CumulativeSum {
     }
 }
 
-fn read_line() -> String {
-    let stdin = io::stdin();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf).unwrap();
-    buf
+struct Scanner {
+    ptr: usize,
+    length: usize,
+    buf: Vec<u8>,
+    small_cache: Vec<u8>,
 }
 
-fn read_values<T>() -> Vec<T> where T: std::str::FromStr, T::Err: std::fmt::Debug {
-    read_line()
-        .split(' ')
-        .map(|a| a.trim().parse().unwrap())
-        .collect()
+impl Scanner {
+    fn new() -> Scanner {
+        Scanner { ptr: 0, length: 0, buf: vec![0; 1024], small_cache: vec![0; 1024] }
+    }
+
+    fn load(&mut self) {
+        use std::io::Read;
+        let mut s = std::io::stdin();
+        self.length = s.read(&mut self.buf).unwrap();
+    }
+
+    fn byte(&mut self) -> u8 {
+        if self.ptr >= self.length {
+            self.ptr = 0;
+            self.load();
+        }
+        self.ptr += 1;
+        return self.buf[self.ptr - 1];
+    }
+
+    fn is_space(b: u8) -> bool { b == b'\n' || b == b'\r' || b == b'\t' || b == b' ' }
+
+    fn read<T>(&mut self) -> T where T: std::str::FromStr, T::Err: std::fmt::Debug, {
+        let mut b = self.byte();
+        while Scanner::is_space(b) {
+            b = self.byte();
+        }
+
+        for pos in 0..self.small_cache.len() {
+            self.small_cache[pos] = b;
+            b = self.byte();
+            if Scanner::is_space(b) {
+                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)]).parse().unwrap();
+            }
+        }
+
+        let mut v = self.small_cache.clone();
+        while !Scanner::is_space(b) {
+            v.push(b);
+            b = self.byte();
+        }
+        return String::from_utf8_lossy(&v).parse().unwrap();
+    }
 }
+
