3d68c10395c66a76f7c1ca6cbaaec8a74448f833
Kenko Nakamura <kenkou.n@gmail.com>
2018-02-27 23:06:49 +0900

    Review ARC090 C

--- atcoder/arc090/arc090_c.rs
+++ atcoder/arc090/arc090_c.rs
@@ -1,144 +1,160 @@
-use std::cmp::Ordering;
 use std::collections::BinaryHeap;
-use std::collections::BTreeSet;
-use std::usize;
 
-const MOD: usize = 1000000007;
+const MOD: usize = 1_000_000_007;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 struct State {
+    v: usize,
     cost: usize,
-    position: usize,
 }
 
 impl Ord for State {
-    fn cmp(&self, other: &State) -> Ordering {
+    fn cmp(&self, other: &State) -> std::cmp::Ordering {
         other.cost.cmp(&self.cost)
     }
 }
 
 impl PartialOrd for State {
-    fn partial_cmp(&self, other: &State) -> Option<Ordering> {
+    fn partial_cmp(&self, other: &State) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-fn main() {
-    let (n, m) = {
-        let v = read_values::<usize>();
-        (v[0], v[1])
-    };
-    let mut graph = (0..n).map(|_| Vec::new()).collect::<Vec<_>>();
-    let (s, t) = {
-        let v = read_values::<usize>();
-        (v[0] - 1, v[1] - 1)
-    };
-
-    for _ in 0..m {
-        let v = read_values::<usize>();
-        let from = v[0] - 1;
-        let to = v[1] - 1;
-        let d = v[2];
-        graph[from].push((to, d));
-        graph[to].push((from, d));
-    }
-
-    let (count_s, dist_s) = first_dijkstra(&graph, s, t);
-    let (count_t, dist_t) = first_dijkstra(&graph, t, s);
-
-    let shortest_dist = dist_s[t];
-
-    let mut edge_set = BTreeSet::new();
-    for from in 0..n {
-        for e in &graph[from] {
-            let (to, c) = *e;
-
-            if dist_s[from] + c + dist_t[to] == shortest_dist
-                && dist_s[from] * 2 < shortest_dist
-                && dist_t[to] * 2 < shortest_dist {
-                edge_set.insert((from, to));
+fn dijkstra(graph: &Vec<Vec<(usize, usize)>>, s: usize, t: usize) -> (Vec<usize>, Vec<usize>) {
+    let mut dist = vec![std::usize::MAX; graph.len()];
+    let mut pattern = vec![0; graph.len()];
+    let mut heap = BinaryHeap::new();
+    dist[s] = 0;
+    pattern[s] = 1;
+    heap.push(State { cost: 0, v: s });
+    while let Some(State { v, cost }) = heap.pop() {
+        if v == t { break; }
+        if cost > dist[v] {
+            continue;
+        }
+        for edge in graph.get(v).unwrap().iter() {
+            let (to, d) = *edge;
+            if cost + d == dist[to] {
+                pattern[to] = (pattern[to] + pattern[v]) % MOD;
+            } else if cost + d < dist[to] {
+                pattern[to] = pattern[v];
+                dist[to] = cost + d;
+                heap.push(State { v: to, cost: cost + d });
             }
         }
     }
 
-    let mut ans = count_s[t] * count_t[s];
-    ans %= MOD;
-    for e in &edge_set {
-        let (from, to) = *e;
-        let (from, to) = if dist_s[from] < dist_s[to] {
-            (from, to)
-        } else {
-            (to, from)
+    (dist, pattern)
+}
+
+fn main() {
+    let mut sc = Scanner::new();
+    let n: usize = sc.read();
+    let m: usize = sc.read();
+    let s: usize = sc.read::<usize>() - 1;
+    let t: usize = sc.read::<usize>() - 1;
+    let mut edges = Vec::new();
+    let mut graph: Vec<Vec<(usize, usize)>> = vec![Vec::new(); n];
+    for _ in 0..m {
+        let u = sc.read::<usize>() - 1;
+        let v = sc.read::<usize>() - 1;
+        let d: usize = sc.read();
+        graph[u].push((v, d));
+        graph[v].push((u, d));
+        edges.push((v, u, d));
+    }
+
+    let (s_dist, s_pattern) = dijkstra(&graph, s, t);
+    let (t_dist, t_pattern) = dijkstra(&graph, t, s);
+
+    let length = s_dist[t];
+
+    let mut ans = (s_pattern[t] * s_pattern[t]) % MOD;
+    for edge in &edges {
+        let (from, to, d) = {
+            let (v, u, d) = *edge;
+            if s_dist[v] < s_dist[u] {
+                (v, u, d)
+            } else {
+                (u, v, d)
+            }
         };
 
-        let mut c = (count_s[from] * count_s[from]) % MOD;
-        c = (c * count_t[to]) % MOD;
-        c = (c * count_t[to]) % MOD;
-        ans = (ans + MOD - c) % MOD;
+        if s_dist[to] != s_dist[from] + d { continue; }
+        if s_dist[from] + d + t_dist[to] != length { continue; }
+
+        if s_dist[from] * 2 < length && length < s_dist[to] * 2 {
+            let mut tmp = (s_pattern[from] * t_pattern[to]) % MOD;
+            tmp = (tmp * tmp) % MOD;
+            ans = (ans + MOD - tmp) % MOD;
+        }
     }
 
     for v in 0..n {
-        if dist_s[v] * 2 == shortest_dist {
-            let mut c = (count_s[v] * count_s[v]) % MOD;
-            c = (c * count_t[v]) % MOD;
-            c = (c * count_t[v]) % MOD;
-            ans = (ans + MOD - c) % MOD;
+        if s_dist[v] * 2 == length {
+            let mut tmp = (s_pattern[v] * t_pattern[v]) % MOD;
+            tmp = (tmp * tmp) % MOD;
+            ans = (ans + MOD - tmp) % MOD;
         }
     }
+
     println!("{}", ans);
 }
 
-fn first_dijkstra(graph: &Vec<Vec<(usize, usize)>>, s: usize, t: usize) -> (Vec<usize>, Vec<usize>) {
-    let n = graph.len();
-    let mut count = vec![0; n];
-    count[s] = 1;
-    let mut dist = vec![usize::MAX; n];
-    dist[s] = 0;
-    let mut heap = BinaryHeap::new();
-    heap.push(State { cost: 0, position: s });
+struct Scanner {
+    ptr: usize,
+    length: usize,
+    buf: Vec<u8>,
+    small_cache: Vec<u8>,
+}
 
-    let mut vis = vec![false; n];
-
-    while let Some(State { cost, position }) = heap.pop() {
-        if cost > dist[t] {
-            break;
-        }
-
-        if vis[position] {
-            continue;
-        }
-        vis[position] = true;
-
-        for e in &graph[position] {
-            let (to, d) = *e;
-            if dist[to] < dist[position] + d {
-                continue;
-            }
-            if dist[to] > dist[position] + d {
-                dist[to] = dist[position] + d;
-                count[to] = 0;
-                heap.push(State { cost: dist[to], position: to });
-            }
-
-            if dist[to] == dist[position] + d {
-                count[to] += count[position];
-                count[to] %= MOD;
-            }
-        }
+impl Scanner {
+    fn new() -> Scanner {
+        Scanner { ptr: 0, length: 0, buf: vec![0; 1024], small_cache: vec![0; 1024] }
     }
-    (count, dist)
+
+    fn load(&mut self) {
+        use std::io::Read;
+        let mut s = std::io::stdin();
+        self.length = s.read(&mut self.buf).unwrap();
+    }
+
+    fn byte(&mut self) -> u8 {
+        if self.ptr >= self.length {
+            self.ptr = 0;
+            self.load();
+            if self.length == 0 {
+                self.buf[0] = b'\n';
+                self.length = 1;
+            }
+        }
+
+        self.ptr += 1;
+        return self.buf[self.ptr - 1];
+    }
+
+    fn is_space(b: u8) -> bool { b == b'\n' || b == b'\r' || b == b'\t' || b == b' ' }
+
+    fn read<T>(&mut self) -> T where T: std::str::FromStr, T::Err: std::fmt::Debug, {
+        let mut b = self.byte();
+        while Scanner::is_space(b) {
+            b = self.byte();
+        }
+
+        for pos in 0..self.small_cache.len() {
+            self.small_cache[pos] = b;
+            b = self.byte();
+            if Scanner::is_space(b) {
+                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)]).parse().unwrap();
+            }
+        }
+
+        let mut v = self.small_cache.clone();
+        while !Scanner::is_space(b) {
+            v.push(b);
+            b = self.byte();
+        }
+        return String::from_utf8_lossy(&v).parse().unwrap();
+    }
 }
 
-fn read_line() -> String {
-    let stdin = std::io::stdin();
-    let mut buf = String::new();
-    stdin.read_line(&mut buf).unwrap();
-    buf
-}
-
-fn read_values<T>() -> Vec<T> where T: std::str::FromStr, T::Err: std::fmt::Debug {
-    read_line()
-        .split(' ')
-        .map(|a| a.trim().parse().unwrap())
-        .collect()
-}
