7f47280dc2f2a2eb53cf64da1bb63b0361c27bfc
Kenko Nakamura <kenkou.n@gmail.com>
2018-03-31 20:13:43 +0900

    Add automatic file creator

---
+++ add_bin.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: ./add_bin.sh [JUDGE] [PROBLEM ID]"
+    exit 1
+fi
+
+judge="$1"
+problem_id="$2"
+path="${judge}/${problem_id}.rs"
+
+if [ -e ${path} ]
+then
+    echo "${path} is already exists"
+else
+    echo "[[bin]]" >> Cargo.toml
+    echo "name = \"${problem_id}\"" >> Cargo.toml
+    echo "path = \"${path}\"" >> Cargo.toml
+    echo "fn main() {}" > ${path}
+    cat common/scanner.rs >> ${path}
+fi
