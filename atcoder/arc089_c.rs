149281b5a6f6921979f7b3f54d30f805e4307918
kenkoooo <kenkou.n@gmail.com>
2018-04-23 19:30:29 +0900

    Solved ARC 089 C again

--- atcoder/arc089_c.rs
+++ atcoder/arc089_c.rs
@@ -1,59 +1,54 @@
-const MAX_D: usize = 100;
-const MAX_C: usize = 100;
+use std::cmp;
+
 const MAX_V: usize = 300;
+const MAX_C: usize = 100;
 
 fn main() {
     let mut sc = Scanner::new();
     let a: usize = sc.read();
     let b: usize = sc.read();
-    let dist: Vec<Vec<usize>> = (0..a).map(|_| (0..b).map(|_| sc.read()).collect()).collect();
+    let dist_table: Vec<Vec<usize>> = (0..a)
+        .map(|_| (0..b).map(|_| sc.read()).collect())
+        .collect();
 
     let n = MAX_V;
-
-    let mut char_edge = Vec::new();
-    let xs = (0..(MAX_D + 1)).map(|i| i).collect::<Vec<_>>();
-    let ys = (0..(MAX_D + 1)).map(|i| n - 1 - i).collect::<Vec<_>>();
-    for i in 0..MAX_D {
-        char_edge.push((xs[i], xs[i + 1], 'X'));
-        char_edge.push((ys[i + 1], ys[i], 'Y'));
-    }
-
     let mut used = vec![vec![false; b]; a];
-    let mut int_edge = Vec::new();
-    for from in 0..(MAX_D + 1) {
-        for to in 0..(MAX_D + 1) {
-            if from + to > MAX_D {
-                continue;
-            }
-            let mut edge_cost = 0;
+    let mut edges = Vec::new();
+    for x_count in 0..(MAX_C + 1) {
+        for y_count in 0..(MAX_C + 1) {
+            let mut edge = 0;
             for i in 0..a {
                 for j in 0..b {
-                    let cost_x = i + 1;
-                    let cost_y = j + 1;
-                    let base_cost = from * cost_x + to * cost_y;
-                    if edge_cost + base_cost < dist[i][j] {
-                        edge_cost = dist[i][j] - base_cost;
+                    let dx = i + 1;
+                    let dy = j + 1;
+                    let base = dx * x_count + dy * y_count;
+                    if dist_table[i][j] < base {
+                        continue;
+                    }
+                    edge = cmp::max(edge, dist_table[i][j] - base);
+                }
+            }
+
+            for i in 0..a {
+                for j in 0..b {
+                    let dx = i + 1;
+                    let dy = j + 1;
+                    let base = dx * x_count + dy * y_count;
+                    if edge + base == dist_table[i][j] {
+                        used[i][j] = true;
                     }
                 }
             }
-            if edge_cost > MAX_C { continue; }
-            int_edge.push((xs[from], ys[to], edge_cost));
 
-            for i in 0..a {
-                for j in 0..b {
-                    let cost_x = i + 1;
-                    let cost_y = j + 1;
-                    let base_cost = from * cost_x + to * cost_y;
-                    assert!(dist[i][j] <= base_cost + edge_cost);
-                    if base_cost + edge_cost == dist[i][j] { used[i][j] = true; }
-                }
-            }
+            let from = x_count;
+            let to = n - 1 - y_count;
+            edges.push((from, to, edge));
         }
     }
 
-    for x in 0..a {
-        for y in 0..b {
-            if !used[x][y] {
+    for i in 0..a {
+        for j in 0..b {
+            if !used[i][j] {
                 println!("Impossible");
                 return;
             }
@@ -61,16 +56,17 @@ fn main() {
     }
 
     println!("Possible");
-    println!("{} {}", n, int_edge.len() + char_edge.len());
-    for e in &char_edge {
-        let (from, to, c) = *e;
-        println!("{} {} {}", from + 1, to + 1, c);
+    println!("{} {}", n, edges.len() + MAX_C * 2);
+    for i in 0..MAX_C {
+        println!("{} {} X", i + 1, i + 2);
     }
-    for e in &int_edge {
-        let (from, to, c) = *e;
-        println!("{} {} {}", from + 1, to + 1, c);
+    for i in 0..MAX_C {
+        println!("{} {} Y", n - 1 - i, n - i);
     }
-    println!("{} {}", xs[0] + 1, ys[0] + 1);
+    for &(from, to, cost) in &edges {
+        println!("{} {} {}", from + 1, to + 1, cost);
+    }
+    println!("{} {}", 1, n);
 }
 
 struct Scanner {
@@ -82,7 +78,12 @@ struct Scanner {
 
 impl Scanner {
     fn new() -> Scanner {
-        Scanner { ptr: 0, length: 0, buf: vec![0; 1024], small_cache: vec![0; 1024] }
+        Scanner {
+            ptr: 0,
+            length: 0,
+            buf: vec![0; 1024],
+            small_cache: vec![0; 1024],
+        }
     }
 
     fn load(&mut self) {
@@ -105,17 +106,27 @@ impl Scanner {
         return self.buf[self.ptr - 1];
     }
 
-    fn is_space(b: u8) -> bool { b == b'\n' || b == b'\r' || b == b'\t' || b == b' ' }
+    fn is_space(b: u8) -> bool {
+        b == b'\n' || b == b'\r' || b == b'\t' || b == b' '
+    }
 
-    fn read<T>(&mut self) -> T where T: std::str::FromStr, T::Err: std::fmt::Debug, {
+    fn read<T>(&mut self) -> T
+    where
+        T: std::str::FromStr,
+        T::Err: std::fmt::Debug,
+    {
         let mut b = self.byte();
-        while Scanner::is_space(b) { b = self.byte(); }
+        while Scanner::is_space(b) {
+            b = self.byte();
+        }
 
         for pos in 0..self.small_cache.len() {
             self.small_cache[pos] = b;
             b = self.byte();
             if Scanner::is_space(b) {
-                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)]).parse().unwrap();
+                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)])
+                    .parse()
+                    .unwrap();
             }
         }
 
@@ -127,4 +138,3 @@ impl Scanner {
         return String::from_utf8_lossy(&v).parse().unwrap();
     }
 }
-
