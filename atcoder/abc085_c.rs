ac55185f12ef57b96c2237ff30d90c00b4cf67db
kenkoooo <kenkou.n@gmail.com>
2018-05-14 14:34:15 -0700

    remove unused import

--- atcoder/abc085_c.rs
+++ atcoder/abc085_c.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::collections::BTreeSet;
 
 fn main() {
     let (n, y) = {
@@ -31,7 +30,11 @@ fn read_line() -> String {
     buf
 }
 
-fn read_values<T>() -> Vec<T> where T: std::str::FromStr, T::Err: std::fmt::Debug {
+fn read_values<T>() -> Vec<T>
+where
+    T: std::str::FromStr,
+    T::Err: std::fmt::Debug,
+{
     read_line()
         .split(' ')
         .map(|a| a.trim().parse().unwrap())
