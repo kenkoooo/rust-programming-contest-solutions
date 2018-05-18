540125171bc41c1ff9f3fe8f8b6ce72878549f1f
kenkoooo <kenkou.n@gmail.com>
2018-05-18 14:34:50 +0900

    Fix build

--- atcoder/agc023_c.rs
+++ atcoder/agc023_c.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+const MOD: usize = 1_000_000_007;
 fn main() {
     let mut sc = Scanner::new();
     let n: usize = sc.read();
@@ -28,82 +29,39 @@ fn main() {
     println!("sum: {}", sum);
 }
 
-pub trait LexicalPermutation {
-    /// Return `true` if the slice was permuted, `false` if it is already
-    /// at the last ordered permutation.
-    fn next_permutation(&mut self) -> bool;
-    /// Return `true` if the slice was permuted, `false` if it is already
-    /// at the first ordered permutation.
-    fn prev_permutation(&mut self) -> bool;
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
 }
 
-impl<T> LexicalPermutation for [T]
-where
-    T: Ord,
-{
-    /// Original author in Rust: Thomas Backman <serenity@exscape.org>
-    fn next_permutation(&mut self) -> bool {
-        // These cases only have 1 permutation each, so we can't do anything.
-        if self.len() < 2 {
-            return false;
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Combination {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
         }
-
-        // Step 1: Identify the longest, rightmost weakly decreasing part of the vector
-        let mut i = self.len() - 1;
-        while i > 0 && self[i - 1] >= self[i] {
-            i -= 1;
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
         }
-
-        // If that is the entire vector, this is the last-ordered permutation.
-        if i == 0 {
-            return false;
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
         }
-
-        // Step 2: Find the rightmost element larger than the pivot (i-1)
-        let mut j = self.len() - 1;
-        while j >= i && self[j] <= self[i - 1] {
-            j -= 1;
+        Combination {
+            fact: fact,
+            inv_fact: inv_fact,
+            modulo: modulo,
         }
-
-        // Step 3: Swap that element with the pivot
-        self.swap(j, i - 1);
-
-        // Step 4: Reverse the (previously) weakly decreasing part
-        self[i..].reverse();
-
-        true
     }
 
-    fn prev_permutation(&mut self) -> bool {
-        // These cases only have 1 permutation each, so we can't do anything.
-        if self.len() < 2 {
-            return false;
-        }
-
-        // Step 1: Identify the longest, rightmost weakly increasing part of the vector
-        let mut i = self.len() - 1;
-        while i > 0 && self[i - 1] <= self[i] {
-            i -= 1;
-        }
-
-        // If that is the entire vector, this is the first-ordered permutation.
-        if i == 0 {
-            return false;
-        }
-
-        // Step 2: Reverse the weakly increasing part
-        self[i..].reverse();
-
-        // Step 3: Find the rightmost element equal to or bigger than the pivot (i-1)
-        let mut j = self.len() - 1;
-        while j >= i && self[j - 1] < self[i - 1] {
-            j -= 1;
-        }
-
-        // Step 4: Swap that element with the pivot
-        self.swap(i - 1, j);
-
-        true
+    pub fn get(&self, x: usize, y: usize) -> usize {
+        assert!(x >= y);
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
     }
 }
 
