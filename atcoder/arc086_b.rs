6f6ba8a3b217149e0627ec6555c279e23d2bc8f9
kenkoooo <kenkou.n@gmail.com>
2018-05-21 17:05:56 +0900

    Solved ARC086B

--- atcoder/arc086_b.rs
+++ atcoder/arc086_b.rs
@@ -1,3 +1,69 @@
+fn main() {
+    let mut sc = Scanner::new();
+    let n: usize = sc.read();
+    let a: Vec<i64> = sc.read_vec(n);
+    let max: i64 = *a.iter().max().unwrap();
+    let min: i64 = *a.iter().min().unwrap();
+
+    let negative_ans = |ans: &mut Vec<(usize, usize)>| {
+        for i in (1..n).rev() {
+            ans.push((i, i - 1));
+        }
+        println!("{}", ans.len());
+        for &mut (from, to) in ans {
+            println!("{} {}", from + 1, to + 1);
+        }
+    };
+    let positive_ans = |ans: &mut Vec<(usize, usize)>| {
+        for i in 1..n {
+            ans.push((i - 1, i));
+        }
+        println!("{}", ans.len());
+        for &mut (from, to) in ans {
+            println!("{} {}", from + 1, to + 1);
+        }
+    };
+    
+    if max < 0 {
+        let mut ans = Vec::new();
+        negative_ans(&mut ans);
+    } else if min >= 0 {
+        let mut ans = Vec::new();
+        positive_ans(&mut ans);
+    } else {
+        let mut ans = Vec::new();
+        if max.abs() > min.abs() {
+            let mut max_i = 0;
+            for i in 0..n {
+                if a[i] == max {
+                    max_i = i;
+                }
+            }
+            for i in 0..n {
+                if i == max_i {
+                    continue;
+                }
+                ans.push((max_i, i));
+            }
+            positive_ans(&mut ans);
+        } else {
+            let mut min_i = 0;
+            for i in 0..n {
+                if a[i] == min {
+                    min_i = i;
+                }
+            }
+            for i in 0..n {
+                if i == min_i {
+                    continue;
+                }
+                ans.push((min_i, i));
+            }
+            negative_ans(&mut ans);
+        }
+    }
+}
+
 struct Scanner {
     ptr: usize,
     length: usize,
