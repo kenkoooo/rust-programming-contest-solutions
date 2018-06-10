1c9d1ac9fdbae4e31e659126f2160886f506d9f7
kenkoooo <kenkou.n@gmail.com>
2018-06-10 19:10:53 +0900

    Solve AGC012B

--- atcoder/agc012_b.rs
+++ atcoder/agc012_b.rs
@@ -1,3 +1,45 @@
+use std::collections::VecDeque;
+fn main() {
+    let mut sc = Scanner::new();
+    let n = sc.usize_read();
+    let m = sc.usize_read();
+    let mut graph = vec![vec![]; n];
+    for _ in 0..m {
+        let a = sc.usize_read() - 1;
+        let b = sc.usize_read() - 1;
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+
+    let q = sc.usize_read();
+    let queries: Vec<(usize, usize, usize)> = (0..q)
+        .map(|_| (sc.usize_read() - 1, sc.usize_read(), sc.usize_read()))
+        .collect();
+
+    let mut ans = vec![0; n];
+    let mut max_d = vec![-1; n];
+    for q in (0..q).rev() {
+        let (v, d, c) = queries[q];
+        let mut queue = VecDeque::new();
+        queue.push_back((v, d as i32));
+        while let Some((v, d)) = queue.pop_front() {
+            if max_d[v] >= d {
+                continue;
+            }
+            max_d[v] = d;
+            if ans[v] == 0 {
+                ans[v] = c;
+            }
+            for &to in &graph[v] {
+                queue.push_back((to, d - 1));
+            }
+        }
+    }
+    for &c in &ans {
+        println!("{}", c);
+    }
+}
+
 struct Scanner {
     ptr: usize,
     length: usize,
