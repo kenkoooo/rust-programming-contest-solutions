cb3b274caa35eedb3ffeebdc24abedb2d9186592
Kenko Nakamura <kenkou.n@gmail.com>
2018-09-29 20:06:04 +0900

    Moved ARC073C

--- archives/atcoder/arc073/arc073_c.rs
+++ archives/atcoder/arc073/arc073_c.rs
@@ -1,128 +1,92 @@
+/// Thank you tanakh!!!
+/// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
 use std::cmp;
 
-fn main() {
-    let mut sc = Scanner::new();
-    let n = sc.read();
-    let mut x: Vec<usize> = vec![0; n];
-    let mut y: Vec<usize> = vec![0; n];
-    for i in 0..n {
-        x[i] = sc.read();
-        y[i] = sc.read();
+const INF: usize = 1e15 as usize;
+
+fn solve1(xy: &Vec<(usize, usize)>) -> usize {
+    let max = xy.iter().map(|&(x, y)| cmp::max(x, y)).max().unwrap();
+    let min = xy.iter().map(|&(x, y)| cmp::min(x, y)).min().unwrap();
+    let max_minimum = xy.iter().map(|&(x, y)| cmp::min(x, y)).max().unwrap();
+    let min_maximum = xy.iter().map(|&(x, y)| cmp::max(x, y)).min().unwrap();
+    (max - min_maximum) * (max_minimum - min)
+}
+
+fn solve2(xy: &Vec<(usize, usize)>) -> usize {
+    let n = xy.len();
+    let mut pairs = vec![];
+    for &(x, y) in xy.iter() {
+        pairs.push((cmp::min(x, y), cmp::max(x, y)));
     }
+    pairs.sort();
 
-    let mut small = vec![];
-    let mut large = vec![];
-    for i in 0..n {
-        small.push(cmp::min(x[i], y[i]));
-        large.push(cmp::max(x[i], y[i]));
-    }
+    let (whole_minimum, _) = pairs[0];
+    let whole_maximum = pairs.iter().map(|&(_, b)| b).max().unwrap();
+    let (mut cur_max, _) = pairs[n - 1];
+    let mut right_min = INF;
 
-    let &small_min = small.iter().min().unwrap();
-    let &small_max = small.iter().max().unwrap();
-    let &large_min = large.iter().min().unwrap();
-    let &large_max = large.iter().max().unwrap();
-    let mut ans = (small_max - small_min) * (large_max - large_min);
-
-    let worst_side = large_max - small_min;
-    let mut max = small_max;
-    let mut min = 1e15 as usize;
-
-    let mut v = vec![];
-    for i in 0..n {
-        v.push((small[i], large[i]));
-    }
-    v.sort();
-
+    let mut ans = (cur_max - pairs[0].0) * (whole_maximum - whole_minimum);
     for i in 0..(n - 1) {
-        let (_, large) = v[i];
-        let (next_small, _) = v[i + 1];
-        min = cmp::min(min, large);
-        max = cmp::max(max, large);
-        let tmp_min = cmp::min(min, next_small);
-        ans = cmp::min(ans, (max - tmp_min) * worst_side);
+        let (_, cur_right) = pairs[i];
+        let (next_left, _) = pairs[i + 1];
+        right_min = cmp::min(right_min, cur_right);
+        let cur_min = cmp::min(right_min, next_left);
+        cur_max = cmp::max(cur_max, cur_right);
+        ans = cmp::min(ans, (whole_maximum - whole_minimum) * (cur_max - cur_min));
     }
-    println!("{}", ans);
+    ans
 }
 
-struct Scanner {
-    ptr: usize,
-    length: usize,
-    buf: Vec<u8>,
-    small_cache: Vec<u8>,
-}
-
-#[allow(dead_code)]
-impl Scanner {
-    fn new() -> Scanner {
-        Scanner {
-            ptr: 0,
-            length: 0,
-            buf: vec![0; 1024],
-            small_cache: vec![0; 1024],
-        }
-    }
-
-    fn load(&mut self) {
-        use std::io::Read;
-        let mut s = std::io::stdin();
-        self.length = s.read(&mut self.buf).unwrap();
-    }
-
-    fn byte(&mut self) -> u8 {
-        if self.ptr >= self.length {
-            self.ptr = 0;
-            self.load();
-            if self.length == 0 {
-                self.buf[0] = b'\n';
-                self.length = 1;
-            }
-        }
-
-        self.ptr += 1;
-        return self.buf[self.ptr - 1];
-    }
-
-    fn is_space(b: u8) -> bool {
-        b == b'\n' || b == b'\r' || b == b'\t' || b == b' '
-    }
-
-    fn read_vec<T>(&mut self, n: usize) -> Vec<T>
-    where
-        T: std::str::FromStr,
-        T::Err: std::fmt::Debug,
-    {
-        (0..n).map(|_| self.read()).collect()
-    }
-
-    fn usize_read(&mut self) -> usize {
-        self.read()
-    }
-
-    fn read<T>(&mut self) -> T
-    where
-        T: std::str::FromStr,
-        T::Err: std::fmt::Debug,
-    {
-        let mut b = self.byte();
-        while Scanner::is_space(b) {
-            b = self.byte();
-        }
-
-        for pos in 0..self.small_cache.len() {
-            self.small_cache[pos] = b;
-            b = self.byte();
-            if Scanner::is_space(b) {
-                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)])
-                    .parse()
-                    .unwrap();
-            }
-        }
-
-        let mut v = self.small_cache.clone();
-        while !Scanner::is_space(b) {
-            v.push(b);
-            b = self.byte();
-        }
-        return String::from_utf8_lossy(&v).parse().unwrap();
-    }
+fn main() {
+    input!(n: usize, xy: [(usize, usize); n]);
+    println!("{}", cmp::min(solve1(&xy), solve2(&xy)));
 }
