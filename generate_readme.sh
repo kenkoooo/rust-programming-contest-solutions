ca9cd4f98bf29b8231459e35984e04713b00bea3
Kenko Nakamura <kenkou.n@gmail.com>
2018-09-29 19:29:29 +0900

    Added README generator

---
+++ generate_readme.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cat << _EOF
+# Solutions in Rust to coding problems
+
+## AtCoder
+
+| Problem | Solution |
+|:--------|:-------- |
+_EOF
+
+for directory in `ls ./archives/atcoder/`
+do
+    for file in `ls ./archives/atcoder/${directory}/`
+    do
+        problem_id=`echo ${file} | sed -e "s/\.rs$//g"`
+        echo "| [${problem_id}](https://beta.atcoder.jp/contests/${directory}/tasks/${problem_id}) | [${file}](./archives/atcoder/${directory}/${file}) |"
+    done
+done
