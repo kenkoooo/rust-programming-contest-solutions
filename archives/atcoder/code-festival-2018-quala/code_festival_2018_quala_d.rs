676def9b8c968821107c3d3799c871c8442b50d8
Kenko Nakamura <kenkou.n@gmail.com>
2018-11-11 22:26:52 +0900

    Solved some problems

--- archives/atcoder/code-festival-2018-quala/code_festival_2018_quala_d.rs
+++ archives/atcoder/code-festival-2018-quala/code_festival_2018_quala_d.rs
@@ -1,121 +1,322 @@
-/// Thank you tanakh!!!
-/// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
-
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
+use self::mod_int::ModInt;
 
 const MOD: usize = 1e9 as usize + 7;
-const INF: usize = MOD;
 
 fn main() {
-    input!(d: usize, f: usize, t: usize, n: usize, x: [usize; n]);
-    let mut x = x;
-    x.insert(0, 0);
-    x.push(d);
+    let mut sc = Scanner::new();
+    let d: usize = sc.read();
+    let f: usize = sc.read();
+    let t: usize = sc.read();
+    let n: usize = sc.read();
+    let mut x: Vec<usize> = sc.read_vec(n);
 
-    let n = x.len();
-
-    let mut pow = vec![1; n + 1];
+    let mut pow2 = vec![ModInt::new(1); n + 1];
     for i in 0..n {
-        pow[i + 1] = (pow[i] * 2) % MOD;
+        pow2[i + 1] = pow2[i] * 2;
     }
 
-    let mut dp = vec![0; n + 1];
-    dp[0] = 1;
+    x.insert(0, 0);
+    x.push(d);
+    let n = x.len();
+
+    let mut dp = vec![ModInt::new(0); n + 1];
+    dp[0] = ModInt::new(1);
+    let mut sum = vec![ModInt::new(0); n + 1];
+
     for i in 0..n {
         if i > 0 {
-            dp[i] += dp[i - 1];
-            dp[i] %= MOD;
+            sum[i] += sum[i - 1];
+            dp[i] = sum[i];
         }
 
         let from = x.upper_bound(x[i] + f - t);
         let to = x.upper_bound(x[i] + f);
-        let skipped = from - i - 1;
-        let combinations = (dp[i] * pow[skipped]) % MOD;
+
+        let skip = from - 1 - i;
+        let combinations = dp[i] * pow2[skip];
 
         if from < dp.len() {
-            dp[from] += combinations;
-            dp[from] %= MOD;
+            sum[from] += combinations;
         }
 
         if to < dp.len() {
-            dp[to] += MOD - combinations;
-            dp[to] %= MOD;
-        }
-        if i == 0 {
-            dp[i] = 0;
+            sum[to] -= combinations;
         }
     }
 
-    dp[0] = 1;
-
-    let mut ans = 0;
+    let mut ans = ModInt::new(0);
     for i in 0..(n - 1) {
-        if d - x[i] <= f - t {
-            ans += dp[i] * pow[n - 2 - i];
-            ans %= MOD;
+        if x[n - 1] - x[i] <= f - t {
+            let skip = (n - 1) - 1 - i;
+            ans += dp[i] * pow2[skip];
         }
     }
 
     ans += dp[n - 1];
-    ans %= MOD;
-
-    println!("{}", ans);
+    println!("{}", ans.value);
 }
 
-trait VecBound<T> {
-    fn upper_bound(&self, key: T) -> usize;
+struct Scanner {
+    ptr: usize,
+    length: usize,
+    buf: Vec<u8>,
+    small_cache: Vec<u8>,
 }
 
-impl VecBound<usize> for Vec<usize> {
-    fn upper_bound(&self, key: usize) -> usize {
-        self.binary_search_by_key(&(key * 2 + 1), |&v| v * 2)
+#[allow(dead_code)]
+impl Scanner {
+    fn new() -> Scanner {
+        Scanner {
+            ptr: 0,
+            length: 0,
+            buf: vec![0; 1024],
+            small_cache: vec![0; 1024],
+        }
+    }
+
+    fn load(&mut self) {
+        use std::io::Read;
+        let mut s = std::io::stdin();
+        self.length = s.read(&mut self.buf).unwrap();
+    }
+
+    fn byte(&mut self) -> u8 {
+        if self.ptr >= self.length {
+            self.ptr = 0;
+            self.load();
+            if self.length == 0 {
+                self.buf[0] = b'\n';
+                self.length = 1;
+            }
+        }
+
+        self.ptr += 1;
+        return self.buf[self.ptr - 1];
+    }
+
+    fn is_space(b: u8) -> bool {
+        b == b'\n' || b == b'\r' || b == b'\t' || b == b' '
+    }
+
+    fn read_vec<T>(&mut self, n: usize) -> Vec<T>
+    where
+        T: std::str::FromStr,
+        T::Err: std::fmt::Debug,
+    {
+        (0..n).map(|_| self.read()).collect()
+    }
+
+    fn read<T>(&mut self) -> T
+    where
+        T: std::str::FromStr,
+        T::Err: std::fmt::Debug,
+    {
+        let mut b = self.byte();
+        while Scanner::is_space(b) {
+            b = self.byte();
+        }
+
+        for pos in 0..self.small_cache.len() {
+            self.small_cache[pos] = b;
+            b = self.byte();
+            if Scanner::is_space(b) {
+                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)])
+                    .parse()
+                    .unwrap();
+            }
+        }
+
+        let mut v = self.small_cache.clone();
+        while !Scanner::is_space(b) {
+            v.push(b);
+            b = self.byte();
+        }
+        return String::from_utf8_lossy(&v).parse().unwrap();
+    }
+}
+
+trait VecBound {
+    fn upper_bound(&self, x: usize) -> usize;
+}
+
+impl VecBound for Vec<usize> {
+    fn upper_bound(&self, x: usize) -> usize {
+        self.binary_search_by_key(&(x * 2 + 1), |&v| v * 2)
             .err()
             .unwrap()
+    }
+}
+pub mod mod_int {
+    use std::ops::{Add, AddAssign, Mul, MulAssign, Rem, Sub, SubAssign};
+
+    #[derive(Copy)]
+    pub struct ModInt<T> {
+        pub value: T,
+        modulo: T,
+    }
+
+    impl<T> Clone for ModInt<T>
+    where
+        T: Copy,
+    {
+        fn clone(&self) -> Self {
+            ModInt {
+                value: self.value,
+                modulo: self.modulo,
+            }
+        }
+
+        fn clone_from(&mut self, source: &ModInt<T>) {
+            self.value = source.value;
+            self.modulo = source.modulo;
+        }
+    }
+
+    impl<T> Add<ModInt<T>> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        type Output = ModInt<T>;
+        fn add(self, rhs: ModInt<T>) -> ModInt<T> {
+            self + rhs.value
+        }
+    }
+
+    impl<T> Add<T> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        type Output = ModInt<T>;
+        fn add(self, rhs: T) -> ModInt<T> {
+            let m = self.modulo;
+            let mut t = rhs + self.value;
+            if t >= m {
+                t = t - m;
+            }
+            ModInt {
+                value: t,
+                modulo: self.modulo,
+            }
+        }
+    }
+
+    impl<T> Sub<T> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        type Output = ModInt<T>;
+        fn sub(self, rhs: T) -> ModInt<T> {
+            let rhs = if rhs >= self.modulo {
+                rhs % self.modulo
+            } else {
+                rhs
+            };
+            let value = if self.value < rhs {
+                self.value + self.modulo
+            } else {
+                self.value
+            };
+            ModInt {
+                value: value - rhs,
+                modulo: self.modulo,
+            }
+        }
+    }
+
+    impl<T> Sub<ModInt<T>> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        type Output = ModInt<T>;
+        fn sub(self, rhs: ModInt<T>) -> ModInt<T> {
+            self - rhs.value
+        }
+    }
+
+    impl<T> AddAssign<T> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        fn add_assign(&mut self, other: T) {
+            *self = *self + other;
+        }
+    }
+    impl<T> AddAssign<ModInt<T>> for ModInt<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+    {
+        fn add_assign(&mut self, other: ModInt<T>) {
+            *self = *self + other;
+        }
+    }
+
+    impl<T> SubAssign<T> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        fn sub_assign(&mut self, other: T) {
+            *self = *self - other;
+        }
+    }
+
+    impl<T> SubAssign<ModInt<T>> for ModInt<T>
+    where
+        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+    {
+        fn sub_assign(&mut self, other: ModInt<T>) {
+            *self = *self - other;
+        }
+    }
+
+    impl<T> Mul<ModInt<T>> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        type Output = ModInt<T>;
+
+        fn mul(self, rhs: ModInt<T>) -> ModInt<T> {
+            self * rhs.value
+        }
+    }
+    impl<T> Mul<T> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        type Output = ModInt<T>;
+
+        fn mul(self, rhs: T) -> ModInt<T> {
+            let t = (self.value * rhs) % self.modulo;
+            ModInt {
+                value: t,
+                modulo: self.modulo,
+            }
+        }
+    }
+
+    impl<T> MulAssign<T> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        fn mul_assign(&mut self, rhs: T) {
+            *self = *self * rhs;
+        }
+    }
+
+    impl<T> MulAssign<ModInt<T>> for ModInt<T>
+    where
+        T: Mul<Output = T> + Rem<Output = T> + Copy,
+    {
+        fn mul_assign(&mut self, rhs: ModInt<T>) {
+            *self = *self * rhs;
+        }
+    }
+
+    impl ModInt<usize> {
+        pub fn new(value: usize) -> Self {
+            ModInt {
+                value: value,
+                modulo: super::MOD,
+            }
+        }
     }
 }
--- archives/atcoder/soundhound2018-summer-final/soundhound2018_summer_final_c.rs
+++ archives/atcoder/soundhound2018-summer-final/soundhound2018_summer_final_c.rs
@@ -1,79 +1,317 @@
-const M: usize = 1e9 as usize + 7;
+/// Thank you tanakh!!!
+/// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let mut s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+use std::ops::*;
+
+const MOD: usize = 1e9 as usize + 7;
 
 fn main() {
-    let mut s = String::new();
-    std::io::stdin().read_line(&mut s).ok();
-    let s: Vec<usize> = s
-        .trim()
-        .split_whitespace()
-        .map(|e| e.parse().ok().unwrap())
-        .collect();
-    let (n, d) = (s[0], s[1]);
-    let mut c = [[0; 31]; 31];
-    for i in 0..n {
-        c[i][0] = 1;
-        for j in 1..(i + 1) {
-            c[i][j] = (c[i - 1][j - 1] + c[i - 1][j]) % M;
-        }
-    }
-
-    let mut pow2 = [0; 999];
-    pow2[0] = 1;
+    input!(n: usize, d: usize);
+    let comb = Combination::new(10000, MOD);
+    let mut pow2 = vec![ModInt::new(1); 10000];
     for i in 1..pow2.len() {
-        pow2[i] = (pow2[i - 1] * 2) % M;
+        pow2[i] = pow2[i - 1] * 2;
     }
 
-    let mut dp = [[[0; 31]; 31]; 30];
-    dp[0][1][1] = 1;
+    let mut dp = vec![vec![vec![ModInt::new(0); n + 1]; n + 1]; n + 1];
+    dp[0][1][1] = ModInt::new(1);
 
     for dist in 1..n {
-        for used in 1..(n + 1) {
-            for next in 1..n {
-                if used + next > n {
-                    continue;
-                }
-                for last_used in 1..(used + 1) {
-                    let unused = if dist <= d { n - used - 1 } else { n - used };
-                    let selecting = if dist == d { next - 1 } else { next };
-                    if unused < selecting {
+        for used in 1..n {
+            for last in 1..(n + 1) {
+                for next in 1..(n + 1) {
+                    if used + next > n {
+                        continue;
+                    }
+                    if used < last {
                         continue;
                     }
 
-                    let mut v = c[unused][selecting];
-                    for _ in 0..next {
-                        v *= (1 << last_used) - 1;
-                        v %= M;
+                    let selectable = if dist <= d { n - used - 1 } else { n - used };
+                    let selecting = if dist == d { next - 1 } else { next };
+                    if selectable < selecting {
+                        continue;
                     }
 
-                    v *= pow2[next * (next - 1) / 2];
-                    v %= M;
+                    let c = comb.get(selectable, selecting);
 
-                    dp[dist][used + next][next] += (dp[dist - 1][used][last_used] * v) % M;
-                    dp[dist][used + next][next] %= M;
+                    let available_edges = next * (next - 1) / 2;
+                    let in_next = pow2[available_edges];
+
+                    let from_last = pow2[last] - 1;
+                    let last_to_next = pow(from_last, next);
+
+                    dp[dist][used + next][next] +=
+                        dp[dist - 1][used][last] * c * in_next * last_to_next;
                 }
             }
         }
     }
 
-    let mut ans = 0;
+    let mut ans = ModInt::new(0);
     for dist in d..n {
         for used in 1..(n + 1) {
-            let mut count = 0;
             for last in 1..(used + 1) {
-                count += dp[dist][used][last];
-                count %= M;
+                let free_vertices = n - used;
+                if free_vertices == 0 {
+                    ans += dp[dist][used][last];
+                } else {
+                    let available_edges = free_vertices * (free_vertices - 1) / 2;
+                    ans += dp[dist][used][last] * pow2[available_edges];
+                }
             }
+        }
+    }
+    println!("{}", ans.value);
+}
 
-            let unused = n - used;
-            let unconnected = if unused > 0 {
-                pow2[unused * (unused - 1) / 2]
-            } else {
-                1
-            };
-            ans += (count * unconnected) % M;
-            ans %= M;
+fn pow(x: ModInt<usize>, mut e: usize) -> ModInt<usize> {
+    let mut cur = x;
+    let mut result = ModInt::new(1);
+    while e > 0 {
+        if e & 1 == 1 {
+            result *= cur;
+        }
+        e >>= 1;
+        cur = cur * cur;
+    }
+    result
+}
+
+pub struct Combination {
+    fact: Vec<usize>,
+    inv_fact: Vec<usize>,
+    modulo: usize,
+}
+
+impl Combination {
+    pub fn new(max: usize, modulo: usize) -> Combination {
+        let mut inv = vec![0; max + 1];
+        let mut fact = vec![0; max + 1];
+        let mut inv_fact = vec![0; max + 1];
+        inv[1] = 1;
+        for i in 2..(max + 1) {
+            inv[i] = inv[modulo % i] * (modulo - modulo / i) % modulo;
+        }
+        fact[0] = 1;
+        inv_fact[0] = 1;
+        for i in 0..max {
+            fact[i + 1] = fact[i] * (i + 1) % modulo;
+        }
+        for i in 0..max {
+            inv_fact[i + 1] = inv_fact[i] * inv[i + 1] % modulo;
+        }
+        Combination {
+            fact: fact,
+            inv_fact: inv_fact,
+            modulo: modulo,
         }
     }
 
-    println!("{}", ans);
+    pub fn get(&self, x: usize, y: usize) -> ModInt<usize> {
+        assert!(x >= y, "{}<{}", x, y);
+        let t = self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo;
+        ModInt::new(t)
+    }
+}
+
+#[derive(Copy, Debug)]
+pub struct ModInt<T> {
+    value: T,
+    modulo: T,
+}
+
+impl<T> Clone for ModInt<T>
+where
+    T: Copy,
+{
+    fn clone(&self) -> Self {
+        ModInt {
+            value: self.value,
+            modulo: self.modulo,
+        }
+    }
+
+    fn clone_from(&mut self, source: &ModInt<T>) {
+        self.value = source.value;
+        self.modulo = source.modulo;
+    }
+}
+
+impl<T> Add<ModInt<T>> for ModInt<T>
+where
+    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+{
+    type Output = ModInt<T>;
+    fn add(self, rhs: ModInt<T>) -> ModInt<T> {
+        self + rhs.value
+    }
+}
+
+impl<T> Add<T> for ModInt<T>
+where
+    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+{
+    type Output = ModInt<T>;
+    fn add(self, rhs: T) -> ModInt<T> {
+        let m = self.modulo;
+        let mut t = rhs + self.value;
+        if t >= m {
+            t = t - m;
+        }
+        ModInt {
+            value: t,
+            modulo: self.modulo,
+        }
+    }
+}
+
+impl<T> Sub<T> for ModInt<T>
+where
+    T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+{
+    type Output = ModInt<T>;
+    fn sub(self, rhs: T) -> ModInt<T> {
+        let rhs = if rhs >= self.modulo {
+            rhs % self.modulo
+        } else {
+            rhs
+        };
+        let value = if self.value < rhs {
+            self.value + self.modulo
+        } else {
+            self.value
+        };
+        ModInt {
+            value: value - rhs,
+            modulo: self.modulo,
+        }
+    }
+}
+
+impl<T> Sub<ModInt<T>> for ModInt<T>
+where
+    T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
+{
+    type Output = ModInt<T>;
+    fn sub(self, rhs: ModInt<T>) -> ModInt<T> {
+        self - rhs.value
+    }
+}
+
+impl<T> AddAssign<T> for ModInt<T>
+where
+    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+{
+    fn add_assign(&mut self, other: T) {
+        *self = *self + other;
+    }
+}
+impl<T> AddAssign<ModInt<T>> for ModInt<T>
+where
+    T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
+{
+    fn add_assign(&mut self, other: ModInt<T>) {
+        *self = *self + other;
+    }
+}
+
+impl<T> Mul<ModInt<T>> for ModInt<T>
+where
+    T: Mul<Output = T> + Rem<Output = T> + Copy,
+{
+    type Output = ModInt<T>;
+
+    fn mul(self, rhs: ModInt<T>) -> ModInt<T> {
+        self * rhs.value
+    }
+}
+impl<T> Mul<T> for ModInt<T>
+where
+    T: Mul<Output = T> + Rem<Output = T> + Copy,
+{
+    type Output = ModInt<T>;
+
+    fn mul(self, rhs: T) -> ModInt<T> {
+        let t = (self.value * rhs) % self.modulo;
+        ModInt {
+            value: t,
+            modulo: self.modulo,
+        }
+    }
+}
+
+impl<T> MulAssign<T> for ModInt<T>
+where
+    T: Mul<Output = T> + Rem<Output = T> + Copy,
+{
+    fn mul_assign(&mut self, rhs: T) {
+        *self = *self * rhs;
+    }
+}
+
+impl<T> MulAssign<ModInt<T>> for ModInt<T>
+where
+    T: Mul<Output = T> + Rem<Output = T> + Copy,
+{
+    fn mul_assign(&mut self, rhs: ModInt<T>) {
+        *self = *self * rhs;
+    }
+}
+
+impl ModInt<usize> {
+    pub fn new(v: usize) -> Self {
+        ModInt {
+            value: v,
+            modulo: MOD,
+        }
+    }
 }
