c413a8995cd531f8e42efd2484d5be7daf6bdbf7
kenkoooo <kenkou.n@gmail.com>
2018-11-29 12:40:28 +0900

    Solved ARC086 again

--- archives/atcoder/arc086/arc086_b.rs
+++ archives/atcoder/arc086/arc086_b.rs
@@ -1,111 +1,57 @@
-/// Thank you tanakh!!!
-/// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
-
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
 fn main() {
-    input!(n: usize, a: [i64; n]);
-    let mut init_check = true;
-    for i in 1..n {
-        if a[i] < a[i - 1] {
-            init_check = false;
-            break;
-        }
-    }
-    if init_check {
-        println!("0");
-        return;
-    }
+    let sc = std::io::stdin();
+    let mut sc = Scanner { reader: sc.lock() };
+    let n = sc.read();
+    let a: Vec<i64> = sc.read_vec(n);
+    let abs_max = (0..n)
+        .map(|i| (a[i].abs(), i))
+        .max()
+        .map(|(_, i)| i)
+        .unwrap();
+    let mut b: Vec<i64> = a.iter().map(|&ai| ai + a[abs_max]).collect();
 
-    let mut max_i = 0;
-    let mut min_i = 0;
+    let all_positive = b.iter().all(|&x| x >= 0);
+    println!("{}", 2 * n - 1);
     for i in 0..n {
-        if a[i] > a[max_i] {
-            max_i = i;
-        }
-        if a[i] < a[min_i] {
-            min_i = i;
-        }
+        println!("{} {}", abs_max + 1, i + 1);
     }
-
-    let mut a = a;
-    let mut ans = vec![];
-    if a[max_i].abs() >= a[min_i].abs() {
-        assert!(a[max_i] > 0);
-        for i in 0..n {
-            if a[i] < 0 {
-                a[i] += a[max_i];
-                ans.push((i, max_i));
-            }
-        }
+    if all_positive {
         for i in 1..n {
-            a[i] += a[i - 1];
-            ans.push((i, i - 1));
-            assert!(a[i - 1] <= a[i]);
+            println!("{} {}", i, i + 1);
+            b[i] += b[i - 1];
         }
     } else {
-        assert!(a[min_i] < 0);
-        for i in 0..n {
-            if a[i] > 0 {
-                a[i] += a[min_i];
-                ans.push((i, min_i));
-            }
-        }
         for i in (1..n).rev() {
-            a[i - 1] += a[i];
-            ans.push((i - 1, i));
-            assert!(a[i - 1] <= a[i]);
+            println!("{} {}", i + 1, i);
+            b[i - 1] += b[i];
         }
     }
+}
 
-    println!("{}", ans.len());
-    for &(y, x) in ans.iter() {
-        println!("{} {}", x + 1, y + 1);
+pub struct Scanner<R> {
+    reader: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
     }
 }
