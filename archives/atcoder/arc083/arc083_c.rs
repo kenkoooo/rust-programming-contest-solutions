53fc146ac998ff948d326782fbac0dedcb8b8ee3
kenkoooo <kenkou.n@gmail.com>
2018-12-03 12:06:21 +0900

    Solved ARC083 E again

--- archives/atcoder/arc083/arc083_c.rs
+++ archives/atcoder/arc083/arc083_c.rs
@@ -1,88 +1,88 @@
-/// Thank you tanakh!!!
-/// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
-
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
 use std::cmp;
+use std::collections::BTreeSet;
 
-const INF: usize = 1e15 as usize;
+const INF: usize = 1e16 as usize;
 
 fn main() {
-    input!(n: usize, p: [usize1; n - 1], x: [usize; n]);
-    let mut tree = vec![vec![]; n];
-    for child in 1..n {
-        let parent = p[child - 1];
-        tree[parent].push(child);
+    let sc = std::io::stdin();
+    let mut sc = Scanner { reader: sc.lock() };
+    let n: usize = sc.read();
+    let p: Vec<usize> = sc.read_vec(n - 1);
+    let x: Vec<usize> = sc.read_vec(n);
+
+    let mut graph = vec![vec![]; n];
+    for i in 1..n {
+        let parent = p[i - 1] - 1;
+        graph[i].push(parent);
+        graph[parent].push(i);
     }
-    if dfs(0, &tree, &x) != INF {
+
+    if dfs(0, 0, &graph, &x) < INF {
         println!("POSSIBLE");
     } else {
         println!("IMPOSSIBLE");
     }
 }
 
-fn dfs(v: usize, tree: &Vec<Vec<usize>>, x: &Vec<usize>) -> usize {
-    let mut dp = vec![INF; x[v] + 1];
-    dp[0] = 0;
-    for &child in tree[v].iter() {
-        let mut next = vec![INF; x[v] + 1];
-        let white = x[child];
-        let black = dfs(child, tree, x);
-        for i in 0..(x[v] + 1) {
-            if i + white <= x[v] {
-                next[i + white] = cmp::min(next[i + white], dp[i] + black);
-            }
-            if i + black <= x[v] {
-                next[i + black] = cmp::min(next[i + black], dp[i] + white);
-            }
-        }
-        dp = next;
+fn dfs(v: usize, p: usize, tree: &Vec<Vec<usize>>, x: &Vec<usize>) -> usize {
+    if tree[v].len() == 1 && v != 0 {
+        return 0;
     }
-    *dp.iter().min().unwrap()
+
+    let mut set: BTreeSet<(usize, usize)> = BTreeSet::new();
+    set.insert((0, 0));
+    for &next in tree[v].iter() {
+        if next != p {
+            let mut next_set = BTreeSet::new();
+            let black = x[next];
+            let white = dfs(next, v, tree, x);
+            for &(b, w) in set.iter() {
+                next_set.insert((b + black, w + white));
+                next_set.insert((w + black, b + white));
+            }
+            set = next_set;
+        }
+    }
+
+    let min1 = set
+        .iter()
+        .filter(|&&(_, y)| y <= x[v])
+        .map(|&(x, _)| x)
+        .min()
+        .unwrap_or(INF);
+    let min2 = set
+        .iter()
+        .filter(|&&(y, _)| y <= x[v])
+        .map(|&(_, y)| y)
+        .min()
+        .unwrap_or(INF);
+    cmp::min(min1, min2)
+}
+
+pub struct Scanner<R> {
+    reader: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
 }
