df85c1cf3e8b4765af78f9de4b741015cbdd7340
kenkoooo <kenkou.n@gmail.com>
2019-01-09 20:30:40 +0900

    Solved

--- archives/atcoder/agc011/agc011_c.rs
+++ archives/atcoder/agc011/agc011_c.rs
@@ -1,112 +1,84 @@
-/// Thank you tanakh!!!
-/// https://qiita.com/tanakh/items/0ba42c7ca36cd29d0ac8
-macro_rules! input {
-    (source = $s:expr, $($r:tt)*) => {
-        let mut iter = $s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-    ($($r:tt)*) => {
-        let mut s = {
-            use std::io::Read;
-            let mut s = String::new();
-            std::io::stdin().read_to_string(&mut s).unwrap();
-            s
-        };
-        let mut iter = s.split_whitespace();
-        input_inner!{iter, $($r)*}
-    };
-}
-
-macro_rules! input_inner {
-    ($iter:expr) => {};
-    ($iter:expr, ) => {};
-
-    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
-        let $var = read_value!($iter, $t);
-        input_inner!{$iter $($r)*}
-    };
-}
-
-macro_rules! read_value {
-    ($iter:expr, ( $($t:tt),* )) => {
-        ( $(read_value!($iter, $t)),* )
-    };
-
-    ($iter:expr, [ $t:tt ; $len:expr ]) => {
-        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
-    };
-
-    ($iter:expr, chars) => {
-        read_value!($iter, String).chars().collect::<Vec<char>>()
-    };
-
-    ($iter:expr, usize1) => {
-        read_value!($iter, usize) - 1
-    };
-
-    ($iter:expr, $t:ty) => {
-        $iter.next().unwrap().parse::<$t>().expect("Parse error")
-    };
-}
-
-use std::collections::VecDeque;
-
 fn main() {
-    input!(n: usize, m: usize, uv: [(usize1, usize1); m]);
+    let sc = std::io::stdin();
+    let mut sc = Scanner { reader: sc.lock() };
+    let n = sc.read();
+    let m = sc.read();
     let mut graph = vec![vec![]; n];
-    for &(u, v) in uv.iter() {
+    for _ in 0..m {
+        let u = sc.read::<usize>() - 1;
+        let v = sc.read::<usize>() - 1;
         graph[u].push(v);
         graph[v].push(u);
     }
 
-    let mut q = VecDeque::new();
-    let mut vis = vec![0; n];
-    let mut components = vec![];
+    let mut pairs = vec![];
+    let mut color = vec![0; n];
     for i in 0..n {
-        if vis[i] != 0 {
-            continue;
+        if color[i] == 0 {
+            let mut count = 0;
+            color[i] = 1;
+            let is_bipartite = coloring(&graph, &mut color, i, &mut count);
+            pairs.push((count, is_bipartite));
         }
-        let mut is_bipartite = true;
-        let mut c = vec![];
-        q.push_back(i);
-        vis[i] = 1;
-        c.push(i);
-        while let Some(v) = q.pop_front() {
-            for &next in graph[v].iter() {
-                if vis[next] != 0 {
-                    if vis[next] == vis[v] {
-                        is_bipartite = false;
-                    }
-                    continue;
-                }
-                vis[next] = if vis[v] == 1 { 2 } else { 1 };
-                q.push_back(next);
-                c.push(next);
-            }
-        }
-
-        components.push((c, is_bipartite));
     }
 
-    let mut cnt_one = 0;
-    let mut cnt_bipartite = 0;
-    let mut cnt_other = 0;
-    for &(ref c, is_bipartite) in components.iter() {
-        if c.len() == 1 {
-            cnt_one += 1;
+    let mut num_single = 0;
+    let mut num_bipartite = 0;
+    for &(count, is_bipartite) in pairs.iter() {
+        if count == 1 {
+            num_single += 1;
         } else if is_bipartite {
-            cnt_bipartite += 1;
-        } else {
-            cnt_other += 1;
+            num_bipartite += 1;
+        }
+    }
+    let num_non_bipartite = pairs.len() - num_single - num_bipartite;
+    let singles = num_single * n * 2 - num_single * num_single;
+    let non_bipartites = num_non_bipartite * (num_bipartite + num_non_bipartite) * 2
+        - num_non_bipartite * num_non_bipartite;
+    let bipartites = num_bipartite * num_bipartite * 2;
+    println!("{}", singles + non_bipartites + bipartites);
+}
+
+fn coloring(graph: &Vec<Vec<usize>>, color: &mut Vec<usize>, v: usize, count: &mut usize) -> bool {
+    *count += 1;
+    let mut result = true;
+    for &next in graph[v].iter() {
+        let next_color = if color[v] == 1 { 2 } else { 1 };
+        if color[next] == 0 {
+            color[next] = next_color;
+            result &= coloring(graph, color, next, count);
+        } else if color[next] != next_color {
+            result = false;
         }
     }
 
-    let mut ans = 0;
-    ans += cnt_one * n * 2 - cnt_one * cnt_one;
-    ans += cnt_bipartite * 2;
-    ans += cnt_other;
-    ans += cnt_bipartite * (cnt_bipartite - 1) * 2;
-    ans += cnt_other * (cnt_other - 1);
-    ans += cnt_other * cnt_bipartite * 2;
-    println!("{}", ans);
+    result
+}
+
+pub struct Scanner<R> {
+    reader: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
 }
