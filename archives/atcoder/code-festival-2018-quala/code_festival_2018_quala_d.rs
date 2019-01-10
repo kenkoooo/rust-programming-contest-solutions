f97d42dd3f2bcb2a2a2afef8425eecbc1eef03be
Kenko Nakamura <kenkou.n@gmail.com>
2019-01-10 10:58:46 +0900

    Solved again

--- archives/atcoder/code-festival-2018-quala/code_festival_2018_quala_d.rs
+++ archives/atcoder/code-festival-2018-quala/code_festival_2018_quala_d.rs
@@ -1,27 +1,29 @@
-use self::mod_int::ModInt;
+use mod_int::ModInt;
 
 const MOD: usize = 1e9 as usize + 7;
 
 fn main() {
-    let mut sc = Scanner::new();
-    let d: usize = sc.read();
+    let s = std::io::stdin();
+    let mut sc = Scanner { reader: s.lock() };
+
+    let d = sc.read();
     let f: usize = sc.read();
     let t: usize = sc.read();
-    let n: usize = sc.read();
+    let n = sc.read();
     let mut x: Vec<usize> = sc.read_vec(n);
-
-    let mut pow2 = vec![ModInt::new(1); n + 1];
-    for i in 0..n {
-        pow2[i + 1] = pow2[i] * 2;
-    }
 
     x.insert(0, 0);
     x.push(d);
     let n = x.len();
 
-    let mut dp = vec![ModInt::new(0); n + 1];
-    dp[0] = ModInt::new(1);
-    let mut sum = vec![ModInt::new(0); n + 1];
+    let mut dp = vec![ModInt(0); n];
+    let mut sum = vec![ModInt(0); n + 1];
+    let mut pow = vec![ModInt(1)];
+    for i in 0..n {
+        let t = pow[i] * 2;
+        pow.push(t);
+    }
+    dp[0] = ModInt(1);
 
     for i in 0..n {
         if i > 0 {
@@ -31,292 +33,180 @@ fn main() {
 
         let from = x.upper_bound(x[i] + f - t);
         let to = x.upper_bound(x[i] + f);
-
         let skip = from - 1 - i;
-        let combinations = dp[i] * pow2[skip];
 
-        if from < dp.len() {
-            sum[from] += combinations;
+        let c = dp[i] * pow[skip];
+        if from < sum.len() {
+            sum[from] += c;
         }
-
-        if to < dp.len() {
-            sum[to] -= combinations;
+        if to < sum.len() {
+            sum[to] -= c;
         }
     }
 
-    let mut ans = ModInt::new(0);
+    let mut ans = ModInt(0);
     for i in 0..(n - 1) {
         if x[n - 1] - x[i] <= f - t {
             let skip = (n - 1) - 1 - i;
-            ans += dp[i] * pow2[skip];
+            ans += dp[i] * pow[skip];
         }
     }
 
     ans += dp[n - 1];
-    println!("{}", ans.value);
+    println!("{}", ans.0);
 }
 
-struct Scanner {
-    ptr: usize,
-    length: usize,
-    buf: Vec<u8>,
-    small_cache: Vec<u8>,
+trait UpperBound<T> {
+    fn upper_bound(&self, x: T) -> usize;
 }
 
-#[allow(dead_code)]
-impl Scanner {
-    fn new() -> Scanner {
-        Scanner {
-            ptr: 0,
-            length: 0,
-            buf: vec![0; 1024],
-            small_cache: vec![0; 1024],
-        }
-    }
-
-    fn load(&mut self) {
-        use std::io::Read;
-        let mut s = std::io::stdin();
-        self.length = s.read(&mut self.buf).unwrap();
-    }
-
-    fn byte(&mut self) -> u8 {
-        if self.ptr >= self.length {
-            self.ptr = 0;
-            self.load();
-            if self.length == 0 {
-                self.buf[0] = b'\n';
-                self.length = 1;
-            }
-        }
-
-        self.ptr += 1;
-        return self.buf[self.ptr - 1];
-    }
-
-    fn is_space(b: u8) -> bool {
-        b == b'\n' || b == b'\r' || b == b'\t' || b == b' '
-    }
-
-    fn read_vec<T>(&mut self, n: usize) -> Vec<T>
-    where
-        T: std::str::FromStr,
-        T::Err: std::fmt::Debug,
-    {
-        (0..n).map(|_| self.read()).collect()
-    }
-
-    fn read<T>(&mut self) -> T
-    where
-        T: std::str::FromStr,
-        T::Err: std::fmt::Debug,
-    {
-        let mut b = self.byte();
-        while Scanner::is_space(b) {
-            b = self.byte();
-        }
-
-        for pos in 0..self.small_cache.len() {
-            self.small_cache[pos] = b;
-            b = self.byte();
-            if Scanner::is_space(b) {
-                return String::from_utf8_lossy(&self.small_cache[0..(pos + 1)])
-                    .parse()
-                    .unwrap();
-            }
-        }
-
-        let mut v = self.small_cache.clone();
-        while !Scanner::is_space(b) {
-            v.push(b);
-            b = self.byte();
-        }
-        return String::from_utf8_lossy(&v).parse().unwrap();
-    }
-}
-
-trait VecBound {
-    fn upper_bound(&self, x: usize) -> usize;
-}
-
-impl VecBound for Vec<usize> {
+impl UpperBound<usize> for Vec<usize> {
     fn upper_bound(&self, x: usize) -> usize {
         self.binary_search_by_key(&(x * 2 + 1), |&v| v * 2)
             .err()
             .unwrap()
     }
 }
+
 pub mod mod_int {
-    use std::ops::{Add, AddAssign, Mul, MulAssign, Rem, Sub, SubAssign};
+    use super::MOD;
+    use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
-    #[derive(Copy)]
-    pub struct ModInt<T> {
-        pub value: T,
-        modulo: T,
+    type Num = usize;
+
+    #[derive(Clone, Copy)]
+    pub struct ModInt<T: Copy + Clone>(pub T);
+
+    impl Add<ModInt<Num>> for ModInt<Num> {
+        type Output = ModInt<Num>;
+        fn add(self, rhs: ModInt<Num>) -> ModInt<Num> {
+            self + rhs.0
+        }
     }
 
-    impl<T> Clone for ModInt<T>
-    where
-        T: Copy,
-    {
-        fn clone(&self) -> Self {
-            ModInt {
-                value: self.value,
-                modulo: self.modulo,
+    impl Add<Num> for ModInt<Num> {
+        type Output = ModInt<Num>;
+        fn add(self, rhs: Num) -> ModInt<Num> {
+            let mut t = rhs + self.0;
+            if t >= MOD {
+                t = t - MOD;
             }
-        }
-
-        fn clone_from(&mut self, source: &ModInt<T>) {
-            self.value = source.value;
-            self.modulo = source.modulo;
+            ModInt(t)
         }
     }
 
-    impl<T> Add<ModInt<T>> for ModInt<T>
-    where
-        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-    {
-        type Output = ModInt<T>;
-        fn add(self, rhs: ModInt<T>) -> ModInt<T> {
-            self + rhs.value
+    impl Sub<Num> for ModInt<Num> {
+        type Output = ModInt<Num>;
+        fn sub(self, rhs: Num) -> ModInt<Num> {
+            let rhs = if rhs >= MOD { rhs % MOD } else { rhs };
+            let value = if self.0 < rhs { self.0 + MOD } else { self.0 };
+            ModInt(value - rhs)
         }
     }
 
-    impl<T> Add<T> for ModInt<T>
-    where
-        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-    {
-        type Output = ModInt<T>;
-        fn add(self, rhs: T) -> ModInt<T> {
-            let m = self.modulo;
-            let mut t = rhs + self.value;
-            if t >= m {
-                t = t - m;
-            }
-            ModInt {
-                value: t,
-                modulo: self.modulo,
-            }
+    impl Sub<ModInt<Num>> for ModInt<Num> {
+        type Output = ModInt<Num>;
+        fn sub(self, rhs: ModInt<Num>) -> ModInt<Num> {
+            self - rhs.0
         }
     }
 
-    impl<T> Sub<T> for ModInt<T>
-    where
-        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
-    {
-        type Output = ModInt<T>;
-        fn sub(self, rhs: T) -> ModInt<T> {
-            let rhs = if rhs >= self.modulo {
-                rhs % self.modulo
-            } else {
-                rhs
-            };
-            let value = if self.value < rhs {
-                self.value + self.modulo
-            } else {
-                self.value
-            };
-            ModInt {
-                value: value - rhs,
-                modulo: self.modulo,
-            }
-        }
-    }
-
-    impl<T> Sub<ModInt<T>> for ModInt<T>
-    where
-        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
-    {
-        type Output = ModInt<T>;
-        fn sub(self, rhs: ModInt<T>) -> ModInt<T> {
-            self - rhs.value
-        }
-    }
-
-    impl<T> AddAssign<T> for ModInt<T>
-    where
-        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-    {
-        fn add_assign(&mut self, other: T) {
+    impl AddAssign<Num> for ModInt<Num> {
+        fn add_assign(&mut self, other: Num) {
             *self = *self + other;
         }
     }
-    impl<T> AddAssign<ModInt<T>> for ModInt<T>
-    where
-        T: Add<Output = T> + Sub<Output = T> + Copy + PartialOrd,
-    {
-        fn add_assign(&mut self, other: ModInt<T>) {
+    impl AddAssign<ModInt<Num>> for ModInt<Num> {
+        fn add_assign(&mut self, other: ModInt<Num>) {
             *self = *self + other;
         }
     }
 
-    impl<T> SubAssign<T> for ModInt<T>
-    where
-        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
-    {
-        fn sub_assign(&mut self, other: T) {
+    impl SubAssign<Num> for ModInt<Num> {
+        fn sub_assign(&mut self, other: Num) {
             *self = *self - other;
         }
     }
 
-    impl<T> SubAssign<ModInt<T>> for ModInt<T>
-    where
-        T: PartialOrd + Copy + Add<Output = T> + Sub<Output = T> + Rem<Output = T>,
-    {
-        fn sub_assign(&mut self, other: ModInt<T>) {
+    impl SubAssign<ModInt<Num>> for ModInt<Num> {
+        fn sub_assign(&mut self, other: ModInt<Num>) {
             *self = *self - other;
         }
     }
 
-    impl<T> Mul<ModInt<T>> for ModInt<T>
-    where
-        T: Mul<Output = T> + Rem<Output = T> + Copy,
-    {
-        type Output = ModInt<T>;
+    impl Mul<ModInt<Num>> for ModInt<Num> {
+        type Output = ModInt<Num>;
 
-        fn mul(self, rhs: ModInt<T>) -> ModInt<T> {
-            self * rhs.value
+        fn mul(self, rhs: ModInt<Num>) -> ModInt<Num> {
+            self * rhs.0
         }
     }
-    impl<T> Mul<T> for ModInt<T>
-    where
-        T: Mul<Output = T> + Rem<Output = T> + Copy,
-    {
-        type Output = ModInt<T>;
+    impl Mul<Num> for ModInt<Num> {
+        type Output = ModInt<Num>;
 
-        fn mul(self, rhs: T) -> ModInt<T> {
-            let t = (self.value * rhs) % self.modulo;
-            ModInt {
-                value: t,
-                modulo: self.modulo,
-            }
+        fn mul(self, rhs: Num) -> ModInt<Num> {
+            let t = (self.0 * rhs) % MOD;
+            ModInt(t)
         }
     }
 
-    impl<T> MulAssign<T> for ModInt<T>
-    where
-        T: Mul<Output = T> + Rem<Output = T> + Copy,
-    {
-        fn mul_assign(&mut self, rhs: T) {
+    impl MulAssign<Num> for ModInt<Num> {
+        fn mul_assign(&mut self, rhs: Num) {
             *self = *self * rhs;
         }
     }
 
-    impl<T> MulAssign<ModInt<T>> for ModInt<T>
-    where
-        T: Mul<Output = T> + Rem<Output = T> + Copy,
-    {
-        fn mul_assign(&mut self, rhs: ModInt<T>) {
+    impl MulAssign<ModInt<Num>> for ModInt<Num> {
+        fn mul_assign(&mut self, rhs: ModInt<Num>) {
             *self = *self * rhs;
         }
     }
 
-    impl ModInt<usize> {
-        pub fn new(value: usize) -> Self {
-            ModInt {
-                value: value,
-                modulo: super::MOD,
-            }
+    impl ModInt<Num> {
+        pub fn new(value: Num) -> Self {
+            ModInt(value)
         }
+
+        pub fn pow(self, e: usize) -> ModInt<Num> {
+            let mut result = ModInt::new(1);
+            let mut cur = self;
+            let mut e = e;
+            while e > 0 {
+                if e & 1 == 1 {
+                    result *= cur;
+                }
+                e >>= 1;
+                cur *= cur;
+            }
+            result
+        }
+    }
+}
+
+pub struct Scanner<R> {
+    reader: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
     }
 }
