28578832a79ce7474805d6f99f1ea110ae2f3648
Kenko Nakamura <kenkou.n@gmail.com>
2019-01-12 20:23:58 +0900

    Solved again

--- archives/atcoder/arc096/arc096_b.rs
+++ archives/atcoder/arc096/arc096_b.rs
@@ -1,84 +1,69 @@
 use std::cmp;
 
 fn main() {
-    let stdin = std::io::stdin();
-    let mut sc = scanner::Scanner::new(stdin.lock());
-    let n: usize = sc.read();
+    let s = std::io::stdin();
+    let mut sc = Scanner { reader: s.lock() };
+
+    let n = sc.read();
     let c: i64 = sc.read();
-    let mut x: Vec<i64> = vec![0; n];
-    let mut v: Vec<i64> = vec![0; n];
-    for i in 0..n {
-        x[i] = sc.read();
-        v[i] = sc.read();
-    }
-
-    let mut right_sum = vec![0; n + 1];
-    for i in 0..n {
-        right_sum[i + 1] = right_sum[i] + v[i];
-    }
-
-    let mut left_sum = vec![0; n + 1];
-    for i in 0..n {
-        left_sum[i + 1] = left_sum[i] + v[n - 1 - i];
-    }
+    let xv: Vec<(i64, i64)> = (0..n).map(|_| (sc.read(), sc.read())).collect();
 
     let mut right_max = vec![0; n + 1];
+    let mut right_back = vec![0; n + 1];
+    let mut right_v = 0;
     for i in 0..n {
-        right_max[i + 1] = cmp::max(right_max[i], right_sum[i + 1] - x[i]);
+        let (x, v) = xv[i];
+        right_v += v;
+        right_max[i + 1] = cmp::max(right_max[i], right_v - x);
+        right_back[i + 1] = cmp::max(right_back[i], right_v - 2 * x);
     }
 
     let mut left_max = vec![0; n + 1];
+    let mut left_back = vec![0; n + 1];
+    let mut left_v = 0;
     for i in 0..n {
-        left_max[i + 1] = cmp::max(left_max[i], left_sum[i + 1] - (c - x[n - 1 - i]));
+        let (x, v) = xv[n - 1 - i];
+        left_v += v;
+        left_max[i + 1] = cmp::max(left_max[i], left_v - (c - x));
+        left_back[i + 1] = cmp::max(left_back[i + 1], left_v - 2 * (c - x));
     }
 
     let mut ans = 0;
     for i in 0..n {
-        let dist = c - x[n - 1 - i];
-        let left = left_sum[i + 1] - dist * 2;
-        ans = cmp::max(ans, left + right_max[n - 1 - i]);
+        ans = cmp::max(ans, left_max[i + 1]);
+        ans = cmp::max(ans, right_max[i + 1]);
+
+        ans = cmp::max(ans, right_back[i + 1] + left_max[n - 1 - i]);
+        ans = cmp::max(ans, left_back[i + 1] + right_max[n - 1 - i]);
     }
-    for i in 0..n {
-        let dist = x[i];
-        let right = right_sum[i + 1] - dist * 2;
-        ans = cmp::max(ans, right + left_max[n - 1 - i]);
-    }
-    ans = cmp::max(ans, left_max[n]);
-    ans = cmp::max(ans, right_max[n]);
+
     println!("{}", ans);
 }
 
-pub mod scanner {
-    use std::fmt::Debug;
-    use std::io::Read;
-    use std::str::{self, FromStr};
+pub struct Scanner<R> {
+    reader: R,
+}
 
-    pub struct Scanner<R: Read> {
-        reader: R,
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
     }
-
-    impl<R: Read> Scanner<R> {
-        pub fn new(reader: R) -> Self {
-            Scanner { reader: reader }
-        }
-
-        pub fn read<T>(&mut self) -> T
-        where
-            T: FromStr,
-            T::Err: Debug,
-        {
-            let buf = self
-                .reader
-                .by_ref()
-                .bytes()
-                .map(|b| b.unwrap())
-                .skip_while(|&b| b == b' ' || b == b'\n')
-                .take_while(|&b| b != b' ' && b != b'\n')
-                .collect::<Vec<_>>();
-
-            unsafe { str::from_utf8_unchecked(&buf) }
-                .parse()
-                .expect("Parse error.")
-        }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
     }
 }
