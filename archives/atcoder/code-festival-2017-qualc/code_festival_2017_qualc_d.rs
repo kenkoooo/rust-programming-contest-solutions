1630037e75ddaeb67c2a3f9ccf2cac0969fb4a7d
kenkoooo <kenkou.n@gmail.com>
2019-01-16 18:05:48 +0900

    Solved again

--- archives/atcoder/code-festival-2017-qualc/code_festival_2017_qualc_d.rs
+++ archives/atcoder/code-festival-2017-qualc/code_festival_2017_qualc_d.rs
@@ -1,10 +1,10 @@
 use std::cmp;
 
-const INF: u32 = 1 << 30;
+const INF: u32 = 1 << 30 as u32;
 
 fn main() {
-    let stdin = std::io::stdin();
-    let mut sc = scanner::Scanner::new(stdin.lock());
+    let sc = std::io::stdin();
+    let mut sc = Scanner { reader: sc.lock() };
     let s: Vec<u32> = sc
         .read::<String>()
         .chars()
@@ -16,62 +16,48 @@ fn main() {
         hash[i + 1] = hash[i] ^ (1 << s[i]);
     }
 
-    let mut prefix: Vec<u32> = vec![0];
-    for i in 0..26 {
-        prefix.push(1 << i);
-    }
-
     let mut opt = vec![INF; n + 1];
-    let mut dp = vec![INF; 1 << 26];
+    let mut min = vec![INF; 1 << 26];
     opt[0] = 0;
-    dp[0] = 0;
+    min[0] = 0;
 
     for i in 0..n {
-        let hash = hash[i + 1];
-        for &prefix in prefix.iter() {
+        let hash = hash[i + 1] as usize;
+        for x in 0..27 {
+            let prefix = if x == 0 { 0 } else { 1 << (x - 1) };
             let suffix = hash ^ prefix;
-            opt[i + 1] = cmp::min(opt[i + 1], dp[suffix as usize] + 1);
+            opt[i + 1] = cmp::min(opt[i + 1], min[suffix] + 1);
         }
-        dp[hash as usize] = cmp::min(dp[hash as usize], opt[i + 1]);
+        min[hash] = cmp::min(min[hash], opt[i + 1]);
     }
 
     println!("{}", opt[n]);
 }
 
-pub mod scanner {
-    use std::fmt::Debug;
-    use std::io::Read;
-    use std::str::{self, FromStr};
+pub struct Scanner<R> {
+    reader: R,
+}
 
-    pub struct Scanner<R: Read> {
-        reader: R,
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .reader
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n')
+            .take_while(|&b| b != b' ' && b != b'\n')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
     }
-
-    impl<R: Read> Scanner<R> {
-        pub fn new(reader: R) -> Self {
-            Scanner { reader: reader }
-        }
-
-        pub fn read<T>(&mut self) -> T
-        where
-            T: FromStr,
-            T::Err: Debug,
-        {
-            let buf: Vec<u8> = self
-                .reader
-                .by_ref()
-                .bytes()
-                .map(|b| b.unwrap())
-                .skip_while(|&b| (b as char).is_whitespace())
-                .take_while(|&b| !(b as char).is_whitespace())
-                .fold(Vec::new(), |mut buf, c| {
-                    buf.push(c);
-                    buf
-                });
-
-            unsafe { str::from_utf8_unchecked(&buf) }
-                .parse()
-                .expect("Parse error.")
-        }
+    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
     }
 }
