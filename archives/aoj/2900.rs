2229b4d27b721e44eeb6c55dc4d6bc7702f1c6a8
Kenko Nakamura <kenkou.n@gmail.com>
2019-02-01 02:30:58 +0900

    Solved

--- archives/aoj/2900.rs
+++ archives/aoj/2900.rs
@@ -1,42 +1,49 @@
 use std::cmp;
 
-fn solve(mut a: Vec<i64>) -> usize {
-    let n = a.len();
-    if n < 3 {
-        return 0;
-    }
-    let mut ans = 0;
-    for i in 0..(n - 2) {
-        let mut t = vec![a[i], a[i + 1], a[i + 2]];
-        if i % 2 == 1 {
-            t = t.iter().map(|&a| -a).collect();
-        }
-
-        if t[0] > t[1] {
-            continue;
-        }
-        if (t[0] < t[2] && t[2] < t[1]) || (t[0] < t[1] && t[1] < t[2]) {
-            a.swap(i, i + 1);
-            ans += 1;
-        } else {
-            a.swap(i + 1, i + 2);
-            ans += 1;
-        }
-    }
-    if (a[n - 3] > a[n - 2]) != (a[n - 2] < a[n - 1]) {
-        a.swap(n - 2, n - 1);
-        ans += 1;
-    }
-    ans
-}
-
 fn main() {
     let s = std::io::stdin();
     let mut sc = Scanner { reader: s.lock() };
-    let n: usize = sc.read();
+    let n = sc.read();
     let a: Vec<i64> = sc.read_vec(n);
-    let b: Vec<i64> = a.iter().map(|&a| -a).collect();
-    println!("{}", cmp::min(solve(a), solve(b)));
+    let b = a.iter().map(|&a| -a).collect();
+    let ans1 = solve(a);
+    let ans2 = solve(b);
+    println!("{}", cmp::min(ans1, ans2));
+}
+
+fn solve(mut a: Vec<i64>) -> usize {
+    let n = a.len();
+    let mut ans = 0;
+    for i in 0..(n - 1) {
+        if i % 2 == 1 {
+            a[i] *= -1;
+            a[i + 1] *= -1;
+            if i + 2 < n {
+                a[i + 2] *= -1;
+            }
+        }
+
+        if a[i] < a[i + 1] {
+            if i + 2 < n && a[i + 1] > a[i + 2] && a[i] > a[i + 2] {
+                // m l s => m s l
+                a.swap(i + 1, i + 2);
+            } else {
+                // s m l => m s l
+                // s l m => l s m
+                a.swap(i, i + 1);
+            }
+            ans += 1;
+        }
+
+        if i % 2 == 1 {
+            a[i] *= -1;
+            a[i + 1] *= -1;
+            if i + 2 < n {
+                a[i + 2] *= -1;
+            }
+        }
+    }
+    ans
 }
 
 pub struct Scanner<R> {
--- archives/atcoder/arc074/arc074_c.rs
+++ archives/atcoder/arc074/arc074_c.rs
@@ -1,169 +1,70 @@
-use self::mod_int::ModInt;
 use std::cmp;
 
 const MOD: usize = 1e9 as usize + 7;
 
-fn max(x: usize, y: usize, z: usize) -> usize {
-    cmp::max(cmp::max(x, y), z)
-}
-
 fn main() {
     let s = std::io::stdin();
     let mut sc = Scanner { reader: s.lock() };
-    let n: usize = sc.read();
-    let m: usize = sc.read();
+
+    let n = sc.read();
+    let m = sc.read();
     let mut constraints = vec![vec![]; n + 1];
     for _ in 0..m {
-        let l: usize = sc.read();
-        let r: usize = sc.read();
+        let l = sc.read::<usize>();
+        let r = sc.read::<usize>();
         let x: usize = sc.read();
         constraints[r].push((l, x));
     }
 
-    let is_ok = |x: usize, y: usize, z: usize| -> bool {
-        let max = max(x, y, z);
-        for &(left, count) in constraints[max].iter() {
-            let mut sum = 0;
-            sum += if x >= left { 1 } else { 0 };
-            sum += if y >= left { 1 } else { 0 };
-            sum += if z >= left { 1 } else { 0 };
-            if count != sum {
-                return false;
-            }
-        }
-        true
-    };
-
-    let mut dp = vec![vec![vec![ModInt::new(0); n + 1]; n + 1]; n + 1];
-    dp[0][0][0] = ModInt::new(1);
-    for x in 0..n {
-        for y in 0..n {
-            for z in 0..n {
-                let max = max(x, y, z);
-                let next = max + 1;
-                if is_ok(next, y, z) {
-                    dp[next][y][z] += dp[x][y][z];
-                }
-                if is_ok(x, next, z) {
-                    dp[x][next][z] += dp[x][y][z];
-                }
-                if is_ok(x, y, next) {
-                    dp[x][y][next] += dp[x][y][z];
+    let mut dp = vec![vec![vec![0; n + 1]; n + 1]; n + 1];
+    dp[0][0][0] = 1;
+    for r in 0..n {
+        for g in 0..n {
+            for b in 0..n {
+                let max = max3(r, g, b);
+                let next = vec![(max + 1, g, b), (r, max + 1, b), (r, g, max + 1)];
+                for &(nr, ng, nb) in next.iter() {
+                    let mut ok = true;
+                    for &(left, x) in constraints[max + 1].iter() {
+                        let mut count = 0;
+                        if nr >= left {
+                            count += 1;
+                        }
+                        if ng >= left {
+                            count += 1;
+                        }
+                        if nb >= left {
+                            count += 1;
+                        }
+                        if count != x {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if ok {
+                        dp[nr][ng][nb] += dp[r][g][b];
+                        dp[nr][ng][nb] %= MOD;
+                    }
                 }
             }
         }
     }
 
-    let mut ans = ModInt::new(0);
+    let mut ans = 0;
     for i in 0..n {
         for j in 0..n {
             ans += dp[n][i][j];
             ans += dp[i][n][j];
             ans += dp[i][j][n];
+            ans %= MOD;
         }
     }
 
-    println!("{}", ans.0);
+    println!("{}", ans);
 }
-pub mod mod_int {
-    use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
 
-    type Num = usize;
-    const MOD: Num = super::MOD;
-
-    #[derive(Clone, Copy)]
-    pub struct ModInt<T: Copy + Clone>(pub T);
-
-    impl Add<ModInt<Num>> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn add(self, rhs: ModInt<Num>) -> ModInt<Num> {
-            self + rhs.0
-        }
-    }
-
-    impl Add<Num> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn add(self, rhs: Num) -> ModInt<Num> {
-            let mut t = rhs + self.0;
-            if t >= MOD {
-                t = t - MOD;
-            }
-            ModInt(t)
-        }
-    }
-
-    impl Sub<Num> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn sub(self, rhs: Num) -> ModInt<Num> {
-            let rhs = if rhs >= MOD { rhs % MOD } else { rhs };
-            let value = if self.0 < rhs { self.0 + MOD } else { self.0 };
-            ModInt(value - rhs)
-        }
-    }
-
-    impl Sub<ModInt<Num>> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn sub(self, rhs: ModInt<Num>) -> ModInt<Num> {
-            self - rhs.0
-        }
-    }
-
-    impl AddAssign<Num> for ModInt<Num> {
-        fn add_assign(&mut self, other: Num) {
-            *self = *self + other;
-        }
-    }
-    impl AddAssign<ModInt<Num>> for ModInt<Num> {
-        fn add_assign(&mut self, other: ModInt<Num>) {
-            *self = *self + other;
-        }
-    }
-
-    impl SubAssign<Num> for ModInt<Num> {
-        fn sub_assign(&mut self, other: Num) {
-            *self = *self - other;
-        }
-    }
-
-    impl SubAssign<ModInt<Num>> for ModInt<Num> {
-        fn sub_assign(&mut self, other: ModInt<Num>) {
-            *self = *self - other;
-        }
-    }
-
-    impl Mul<ModInt<Num>> for ModInt<Num> {
-        type Output = ModInt<Num>;
-
-        fn mul(self, rhs: ModInt<Num>) -> ModInt<Num> {
-            self * rhs.0
-        }
-    }
-    impl Mul<Num> for ModInt<Num> {
-        type Output = ModInt<Num>;
-
-        fn mul(self, rhs: Num) -> ModInt<Num> {
-            let t = (self.0 * rhs) % MOD;
-            ModInt(t)
-        }
-    }
-
-    impl MulAssign<Num> for ModInt<Num> {
-        fn mul_assign(&mut self, rhs: Num) {
-            *self = *self * rhs;
-        }
-    }
-
-    impl MulAssign<ModInt<Num>> for ModInt<Num> {
-        fn mul_assign(&mut self, rhs: ModInt<Num>) {
-            *self = *self * rhs;
-        }
-    }
-
-    impl ModInt<Num> {
-        pub fn new(value: Num) -> Self {
-            ModInt(value)
-        }
-    }
+fn max3(x: usize, y: usize, z: usize) -> usize {
+    cmp::max(cmp::max(x, y), z)
 }
 
 pub struct Scanner<R> {
