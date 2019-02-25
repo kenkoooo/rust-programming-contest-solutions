b0bf6620ef6c6fa3d03aef6af35accc9be25ef33
Kenko Nakamura <kenkou.n@gmail.com>
2019-02-25 23:18:56 +0900

    Solved again

--- archives/atcoder/arc100/arc100_c.rs
+++ archives/atcoder/arc100/arc100_c.rs
@@ -1,32 +1,30 @@
 use std::cmp;
-use std::collections::BTreeSet;
 
 fn main() {
     let s = std::io::stdin();
     let mut sc = Scanner { stdin: s.lock() };
-
     let n: usize = sc.read();
-    let a: Vec<usize> = sc.vec(1 << n);
-
-    let mut top = a
-        .iter()
-        .enumerate()
-        .map(|(i, &a)| (a, i))
-        .collect::<Vec<(usize, usize)>>();
+    let a: Vec<u64> = sc.vec(1 << n);
+    let mut max2: Vec<Vec<(u64, usize)>> =
+        a.iter().enumerate().map(|(i, &a)| vec![(a, i)]).collect();
+    for mask in 0..(1 << n) {
+        for i in 0..n {
+            let next = mask | (1 << i);
+            let from = max2[mask].clone();
+            max2[next].extend(from);
+            max2[next].sort();
+            max2[next].reverse();
+            max2[next].dedup();
+            max2[next].resize(2, (0, 0));
+        }
+    }
 
     let mut ans = vec![0; 1 << n];
     for mask in 1..(1 << n) {
-        let mut set = BTreeSet::new();
-        set.insert(top[mask]);
-        for pos in 0..n {
-            let pos_mask = 1 << pos;
-            set.insert(top[mask & (!pos_mask)]);
-        }
-
-        let mut iter = set.into_iter().rev();
-        top[mask] = iter.next().unwrap();
-        let second = iter.next().unwrap();
-        ans[mask] = cmp::max(ans[mask - 1], top[mask].0 + second.0);
+        let (a0, _) = max2[mask][0];
+        let (a1, _) = max2[mask][1];
+        ans[mask] = cmp::max(ans[mask], a0 + a1);
+        ans[mask] = cmp::max(ans[mask], ans[mask - 1]);
         println!("{}", ans[mask]);
     }
 }
@@ -43,8 +41,8 @@ impl<R: std::io::Read> Scanner<R> {
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
-            .skip_while(|&b| b == b' ' || b == b'\n')
-            .take_while(|&b| b != b' ' && b != b'\n')
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
             .collect::<Vec<_>>();
         unsafe { std::str::from_utf8_unchecked(&buf) }
             .parse()
