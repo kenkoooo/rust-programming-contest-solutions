5323b51b6dfe201cb2ff3313ecce2beee7820f3b
Kenko Nakamura <kenkou.n@gmail.com>
2019-02-26 03:06:48 +0900

    Solved

--- archives/atcoder/arc087/arc087_c.rs
+++ archives/atcoder/arc087/arc087_c.rs
@@ -1,109 +1,109 @@
-use std::collections::BTreeSet;
-
-const INF: usize = 1e15 as usize;
+use std::collections::{BTreeMap, BTreeSet};
 
 fn main() {
     let s = std::io::stdin();
-    let mut sc = Scanner { reader: s.lock() };
+    let mut sc = Scanner { stdin: s.lock() };
+
     let n: usize = sc.read();
     let l: usize = sc.read();
-
-    let mut trie = Trie::new();
+    let mut trie = TrieNode::new();
     for _ in 0..n {
-        let s = sc.chars();
+        let s: Vec<usize> = sc
+            .chars()
+            .into_iter()
+            .map(|c| c as usize - '0' as usize)
+            .collect();
         trie.add(&s, 0);
     }
-    let mut count = vec![];
-    trie.count_tree(0, &mut count);
 
-    let mut ans = 0;
-    for grundy in count.iter().map(|&depth| (l - depth).trailing_zeros() + 1) {
-        ans ^= grundy;
+    let mut result = vec![];
+    trie.get_count(0, &mut result);
+
+    let mut grundy: u64 = 0;
+    for &r in result.iter() {
+        let height = l - r;
+        let g = 1 << (height as u32).trailing_zeros();
+        grundy ^= g;
     }
-    println!("{}", if ans == 0 { "Bob" } else { "Alice" });
-
-    // let mut grundy = vec![INF; 31];
-    // grundy[0] = 0;
-    // get(30, &mut grundy);
-    // println!("{:?}", grundy);
+    println!("{}", if grundy != 0 { "Alice" } else { "Bob" });
 }
 
-struct Trie {
-    child: Vec<Option<Trie>>,
+struct TrieNode {
+    child: [Option<Box<TrieNode>>; 2],
 }
 
-impl Trie {
+impl TrieNode {
     fn new() -> Self {
-        Trie {
-            child: vec![None, None],
+        TrieNode {
+            child: [None, None],
         }
     }
 
-    fn count_tree(&self, depth: usize, result: &mut Vec<usize>) {
-        let len = self.child.iter().filter(|c| c.is_some()).count();
-        if len == 1 {
-            result.push(depth);
+    fn add(&mut self, s: &[usize], pos: usize) {
+        if s.len() == pos {
+            return;
+        }
+        if self.child[s[pos]].is_none() {
+            self.child[s[pos]] = Some(Box::new(TrieNode::new()));
+        }
+        self.child[s[pos]].as_mut().unwrap().add(s, pos + 1);
+    }
+
+    fn get_count(&self, height: usize, result: &mut Vec<usize>) {
+        let count = self.child.iter().filter(|c| c.is_none()).count();
+        if count == 1 {
+            result.push(height);
         }
         for child in self.child.iter() {
-            match child {
-                Some(child) => child.count_tree(depth + 1, result),
+            match child.as_ref() {
+                Some(child) => child.get_count(height + 1, result),
                 None => {}
             }
         }
     }
-
-    fn add(&mut self, s: &Vec<char>, pos: usize) {
-        if s.len() == pos {
-            return;
-        }
-        let index = s[pos] as usize - ('0' as usize);
-        self.child[index].get_or_insert(Trie::new()).add(s, pos + 1);
-    }
 }
 
-fn get(i: usize, grundy: &mut Vec<usize>) -> usize {
-    if grundy[i] != INF {
-        return grundy[i];
-    }
-
-    let mut set = BTreeSet::new();
-    for k in 1..(i + 1) {
-        let mut g = 0;
-        for j in k..i {
-            g ^= get(j, grundy);
+fn grundy(h: usize, map: &mut BTreeMap<usize, u32>) -> u32 {
+    match map.get(&h) {
+        Some(&v) => v,
+        None if h == 1 => 1,
+        None => {
+            let mut set = BTreeSet::new();
+            for remove in 1..(h + 1) {
+                let mut g = 0;
+                for i in 1..remove {
+                    g ^= grundy(h - i, map);
+                }
+                set.insert(g);
+            }
+            let g = (0..).skip_while(|i| set.contains(&i)).next().unwrap();
+            map.insert(h, g);
+            g
         }
-        set.insert(g);
     }
-    for j in 0.. {
-        if !set.contains(&j) {
-            grundy[i] = j;
-            break;
-        }
-    }
-    grundy[i]
 }
 
 pub struct Scanner<R> {
-    reader: R,
+    stdin: R,
 }
 
 impl<R: std::io::Read> Scanner<R> {
     pub fn read<T: std::str::FromStr>(&mut self) -> T {
         use std::io::Read;
         let buf = self
-            .reader
+            .stdin
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
-            .skip_while(|&b| b == b' ' || b == b'\n')
-            .take_while(|&b| b != b' ' && b != b'\n')
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
             .collect::<Vec<_>>();
         unsafe { std::str::from_utf8_unchecked(&buf) }
             .parse()
             .ok()
             .expect("Parse error.")
     }
-    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
         (0..n).map(|_| self.read()).collect()
     }
     pub fn chars(&mut self) -> Vec<char> {
