b0eb32f778dc1d30d4a46cb05b02784ec57e8df1
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-29 01:37:09 +0900

    Solved again

--- archives/atcoder/code-festival-2017-qualc/code_festival_2017_qualc_d.rs
+++ archives/atcoder/code-festival-2017-qualc/code_festival_2017_qualc_d.rs
@@ -1,35 +1,31 @@
 use std::cmp;
 
-const INF: u32 = 1e7 as u32;
-
 fn main() {
     let s = std::io::stdin();
     let mut sc = Scanner { stdin: s.lock() };
-    let s = sc
+    let s: Vec<_> = sc
         .chars()
         .into_iter()
         .map(|c| c as usize - 'a' as usize)
-        .collect::<Vec<_>>();
+        .collect();
     let n = s.len();
 
-    let mut min = vec![INF; 1 << 26];
-    min[0] = 0;
-    let mut hash = 0;
+    let mut hash: usize = 0;
+    let mut min: Vec<u32> = vec![n as u32; 1 << 26];
     for i in 0..n {
         hash ^= 1 << s[i];
-        let split_min = (0..26)
-            .map(|i| {
-                let palindrome_prefix = 1 << i;
-                let suffix = hash ^ palindrome_prefix;
-                min[suffix] + 1
-            })
-            .min()
-            .unwrap();
-
-        let dp = cmp::min(split_min, min[hash] + 1);
-        min[hash] = cmp::min(min[hash], dp);
+        let mut ans = n as u32;
+        if hash.count_ones() <= 1 {
+            ans = 1;
+        } else {
+            for j in 0..26 {
+                ans = cmp::min(ans, min[hash ^ (1 << j)] + 1);
+            }
+            ans = cmp::min(ans, min[hash]);
+        }
+        min[hash] = cmp::min(min[hash], ans);
         if i == n - 1 {
-            println!("{}", dp);
+            println!("{}", ans);
         }
     }
 }
@@ -46,8 +42,8 @@ impl<R: std::io::Read> Scanner<R> {
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
-            .skip_while(|&b| b == b' ' || b == b'\n')
-            .take_while(|&b| b != b' ' && b != b'\n')
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
             .collect::<Vec<_>>();
         unsafe { std::str::from_utf8_unchecked(&buf) }
             .parse()
