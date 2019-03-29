20def6841b9a058128abd81700021022bde861f6
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-29 18:46:48 +0900

    Solved some

--- archives/atcoder/arc102/arc102_c.rs
+++ archives/atcoder/arc102/arc102_c.rs
@@ -1,40 +1,46 @@
-use self::mod_int::ModInt;
-use std::cmp;
-
 const MOD: usize = 998244353;
 
 fn main() {
     let s = std::io::stdin();
-    let mut sc = Scanner { reader: s.lock() };
+    let mut sc = Scanner { stdin: s.lock() };
     let k: usize = sc.read();
     let n: usize = sc.read();
-    let combination = Combination::new(1000000, MOD);
+
+    let combination = Combination::new(k + n, MOD);
     for sum in 2..(2 * k + 1) {
-        let ans = solve(sum, k, n, &combination);
-        println!("{}", ans.0);
+        println!("{}", solve(sum, k, n, &combination));
     }
 }
 
-fn solve(sum: usize, k: usize, n: usize, combination: &Combination) -> ModInt<usize> {
-    let max = cmp::min(k, sum / 2);
-    let min = if sum > k { sum - k } else { 1 };
-    let pair_count = max - min + 1;
+fn solve(sum: usize, max_value: usize, num_dice: usize, combination: &Combination) -> usize {
+    let mut pairs = 0;
+    for i in 1.. {
+        if i * 2 > sum {
+            break;
+        }
+        let other = sum - i;
+        if other > max_value {
+            continue;
+        }
+        pairs += 1;
+    }
 
-
-    let mut ans = combination.h(k, n);
-    for used_pair in 1..(pair_count + 1) {
-        if n < 2 * used_pair {
+    let mut ans = combination.h(max_value, num_dice);
+    for used_pairs in 1..(pairs + 1) {
+        if num_dice < used_pairs * 2 {
             break;
         }
 
-        let pair_selection = combination.get(pair_count, used_pair);
-        let other_combination = combination.h(k, n - 2 * used_pair);
-        let count = pair_selection * other_combination;
-        if used_pair % 2 == 1 {
-            ans -= count;
+        let num_other_dice = num_dice - used_pairs * 2;
+        let mut invalid_moves = combination.get(num_other_dice + max_value - 1, num_other_dice)
+            * combination.get(pairs, used_pairs);
+        invalid_moves %= MOD;
+        if used_pairs % 2 == 1 {
+            ans += MOD - invalid_moves;
         } else {
-            ans += count;
+            ans += invalid_moves;
         }
+        ans %= MOD;
     }
     ans
 }
@@ -69,155 +75,37 @@ impl Combination {
         }
     }
 
-    pub fn get(&self, x: usize, y: usize) -> ModInt<usize> {
+    pub fn get(&self, x: usize, y: usize) -> usize {
         assert!(x >= y);
-        let t = self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo;
-        ModInt::new(t)
+        self.fact[x] * self.inv_fact[y] % self.modulo * self.inv_fact[x - y] % self.modulo
     }
 
-    pub fn h(&self, n: usize, r: usize) -> ModInt<usize> {
+    pub fn h(&self, n: usize, r: usize) -> usize {
         self.get(n + r - 1, r)
     }
 }
 
-pub mod mod_int {
-    use super::MOD;
-    use std::ops::{Add, AddAssign, Mul, MulAssign, Sub, SubAssign};
-
-    type Num = usize;
-
-    #[derive(Clone, Copy)]
-    pub struct ModInt<T: Copy + Clone>(pub T);
-
-    impl Add<ModInt<Num>> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn add(self, rhs: ModInt<Num>) -> ModInt<Num> {
-            self + rhs.0
-        }
-    }
-
-    impl Add<Num> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn add(self, rhs: Num) -> ModInt<Num> {
-            let mut t = rhs + self.0;
-            if t >= MOD {
-                t = t - MOD;
-            }
-            ModInt(t)
-        }
-    }
-
-    impl Sub<Num> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn sub(self, rhs: Num) -> ModInt<Num> {
-            let rhs = if rhs >= MOD { rhs % MOD } else { rhs };
-            let value = if self.0 < rhs { self.0 + MOD } else { self.0 };
-            ModInt(value - rhs)
-        }
-    }
-
-    impl Sub<ModInt<Num>> for ModInt<Num> {
-        type Output = ModInt<Num>;
-        fn sub(self, rhs: ModInt<Num>) -> ModInt<Num> {
-            self - rhs.0
-        }
-    }
-
-    impl AddAssign<Num> for ModInt<Num> {
-        fn add_assign(&mut self, other: Num) {
-            *self = *self + other;
-        }
-    }
-
-    impl AddAssign<ModInt<Num>> for ModInt<Num> {
-        fn add_assign(&mut self, other: ModInt<Num>) {
-            *self = *self + other;
-        }
-    }
-
-    impl SubAssign<Num> for ModInt<Num> {
-        fn sub_assign(&mut self, other: Num) {
-            *self = *self - other;
-        }
-    }
-
-    impl SubAssign<ModInt<Num>> for ModInt<Num> {
-        fn sub_assign(&mut self, other: ModInt<Num>) {
-            *self = *self - other;
-        }
-    }
-
-    impl Mul<ModInt<Num>> for ModInt<Num> {
-        type Output = ModInt<Num>;
-
-        fn mul(self, rhs: ModInt<Num>) -> ModInt<Num> {
-            self * rhs.0
-        }
-    }
-
-    impl Mul<Num> for ModInt<Num> {
-        type Output = ModInt<Num>;
-
-        fn mul(self, rhs: Num) -> ModInt<Num> {
-            let t = (self.0 * rhs) % MOD;
-            ModInt(t)
-        }
-    }
-
-    impl MulAssign<Num> for ModInt<Num> {
-        fn mul_assign(&mut self, rhs: Num) {
-            *self = *self * rhs;
-        }
-    }
-
-    impl MulAssign<ModInt<Num>> for ModInt<Num> {
-        fn mul_assign(&mut self, rhs: ModInt<Num>) {
-            *self = *self * rhs;
-        }
-    }
-
-    impl ModInt<Num> {
-        pub fn new(value: Num) -> Self {
-            ModInt(value)
-        }
-
-        pub fn pow(self, e: usize) -> ModInt<Num> {
-            let mut result = ModInt::new(1);
-            let mut cur = self;
-            let mut e = e;
-            while e > 0 {
-                if e & 1 == 1 {
-                    result *= cur;
-                }
-                e >>= 1;
-                cur *= cur;
-            }
-            result
-        }
-    }
-}
-
 pub struct Scanner<R> {
-    reader: R,
+    stdin: R,
 }
 
 impl<R: std::io::Read> Scanner<R> {
     pub fn read<T: std::str::FromStr>(&mut self) -> T {
         use std::io::Read;
         let buf = self
-            .reader
+            .stdin
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
-            .skip_while(|&b| b == b' ' || b == b'\n')
-            .take_while(|&b| b != b' ' && b != b'\n')
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
             .collect::<Vec<_>>();
         unsafe { std::str::from_utf8_unchecked(&buf) }
             .parse()
             .ok()
             .expect("Parse error.")
     }
-    pub fn read_vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
         (0..n).map(|_| self.read()).collect()
     }
     pub fn chars(&mut self) -> Vec<char> {
