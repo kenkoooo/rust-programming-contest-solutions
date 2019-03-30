5af975276e0f9fb35fac097e0ca4c60b60bcd904
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-30 14:50:25 +0900

    Solve again

--- archives/atcoder/agc030/agc030_b.rs
+++ archives/atcoder/agc030/agc030_b.rs
@@ -4,43 +4,39 @@ fn main() {
     let s = std::io::stdin();
     let mut sc = Scanner { stdin: s.lock() };
     let l: u64 = sc.read();
-    let n: usize = sc.read();
-    let x: Vec<u64> = sc.vec(n);
-
+    let n = sc.read();
+    let mut x: Vec<u64> = sc.vec(n);
     let ans1 = solve(&x, l);
-    let x = x.into_iter().rev().map(|x| l - x).collect();
+    x = x.into_iter().rev().map(|x| l - x).collect();
     let ans2 = solve(&x, l);
-
     println!("{}", cmp::max(ans1, ans2));
 }
 
 fn solve(x: &Vec<u64>, l: u64) -> u64 {
     let n = x.len();
-
     let mut left_sum = vec![0; n + 1];
+    for i in 0..n {
+        left_sum[i + 1] = left_sum[i] + (l - x[n - 1 - i]);
+    }
     let mut right_sum = vec![0; n + 1];
     for i in 0..n {
         right_sum[i + 1] = right_sum[i] + x[i];
-        left_sum[i + 1] = left_sum[i] + (l - x[n - 1 - i]);
     }
 
-    let mut ans = 0;
+    let mut result = 0;
     for i in 0..n {
-        let rest = n - (i + 1);
-        let left = (rest + 1) / 2;
-        let right = n - left;
-        let right_sum = right_sum[right] - right_sum[i + 1];
-        let dist = left_sum[left] * 2 + right_sum * 2 + x[i] * 2;
-        let last = if rest % 2 == 0 {
-            x[right - 1]
+        let rest = n - i;
+        let mut ans = 0;
+        ans += (right_sum[rest / 2 + i] - right_sum[i]) * 2;
+        ans += left_sum[rest / 2] * 2;
+        if rest % 2 == 0 && rest / 2 > 0 {
+            ans -= l - x[n - rest / 2];
         } else {
-            l - x[right]
-        };
-
-        ans = cmp::max(ans, dist - last);
+            ans += x[rest / 2 + i];
+        }
+        result = cmp::max(result, ans);
     }
-
-    ans
+    result
 }
 
 pub struct Scanner<R> {
@@ -55,8 +51,8 @@ impl<R: std::io::Read> Scanner<R> {
             .by_ref()
             .bytes()
             .map(|b| b.unwrap())
-            .skip_while(|&b| b == b' ' || b == b'\n')
-            .take_while(|&b| b != b' ' && b != b'\n')
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
             .collect::<Vec<_>>();
         unsafe { std::str::from_utf8_unchecked(&buf) }
             .parse()
